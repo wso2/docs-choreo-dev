ab228911bccbdac93754b1a1b5979423c0a3ef28
afkham <afkham@gmail.com>
2020-04-09 15:12:45 +0530

    Path fix

--- setup/gen-sealed-ingress-cert.sh
+++ setup/gen-sealed-ingress-cert.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-mkdir out
-kubectl create -n kube-system secret tls ingress-cert --key tls.key --cert tls.crt --dry-run -o yaml > out/ingress-cert.yaml
-kubeseal --scope strict --cert sealed-secret.crt < out/ingress-cert.yaml -o yaml  > out/sealed-ingress-cert.yaml
 
-kubectl apply -f out/sealed-ingress-cert.yaml
-kubectl apply -f nginx-ingress-controller.yaml
+for env in "dev" "staging" "prod"
+do
+    mkdir -p out/$env
+    kubectl create -n $env-choreo-system secret tls ingress-cert --key tls.key --cert tls.crt --dry-run=client -o yaml \
+            > out/$env/ingress-cert.yaml
+    kubeseal --scope strict < out/$env/ingress-cert.yaml -o yaml  > out/$env/sealed-ingress-cert.yaml
+    cp out/$env/sealed-ingress-cert.yaml ../$env/
+    echo "Sealed secret ingress cert generated and copied to "$env
+done
+
+rm -rf out
+
+
