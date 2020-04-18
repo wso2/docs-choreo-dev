4aaf8da9983a1f8acd70765a10be2a8f1eebed6a
afkham <afkham@gmail.com>
2020-04-18 19:10:38 +0530

    Minor improvement to handle file names with dots

--- databases/build.sh
+++ databases/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for db in $(ls -1 scripts | sed -e 's/\..*$//'); do
+for db in $(ls scripts | sed 's/\(.*\)\..*/\1/'); do
     docker build --build-arg DATABASE=${db} -t choreoipaas/${db}:latest .
     docker push choreoipaas/${db}:latest
 done
