ae41df42ab915a9c1e8b4431a8f9793bd538490c
afkham <afkham@gmail.com>
2020-04-19 16:30:11 +0530

    Add reloader which allows dynamic reloading of deployments when secrets or config maps change

--- setup/setup.sh
+++ setup/setup.sh
@@ -25,6 +25,10 @@ done
 outdir=out
 mkdir $outdir
 
+############## Install Reloader
+echo "--- Installing Reloader..."
+k apply -n kube-system -f reloader.yaml
+
 ############## Install nginx ingress
 echo "--- Installing nginx ingress..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
