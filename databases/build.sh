a0a3592f6b460401096fc750ce63f9530359d9a2
afkham <afkham@gmail.com>
2020-05-29 17:05:20 +0530

    Add fk_last_version_id FK constraint

--- databases/build.sh
+++ databases/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t choreoipaas/choreo_db:0.2.2 .
-docker push choreoipaas/choreo_db:0.2.2
+docker build -t choreoipaas/choreo_db:0.2.3 .
+docker push choreoipaas/choreo_db:0.2.3
