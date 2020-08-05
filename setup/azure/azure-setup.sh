a6db18e66910fc8a71ac5333c3fd9673c3590280
dhananjaya-senanayake <dhananjaya+1@wso2.com>
2020-08-05 07:26:13 +0530

    fix linkerd helm installation command

--- setup/azure/azure-setup.sh
+++ setup/azure/azure-setup.sh
@@ -118,9 +118,9 @@ helm repo add linkerd https://helm.linkerd.io/stable
 helm repo update
 helm upgrade --install linkerd2 --wait \
      --set-file global.identityTrustAnchorsPEM=/tmp/ca.crt \
-     -f values.yaml -f ha-values.yaml \
-     -n linkerd \
-     --version 2.8.1
+     linkerd/linkerd2 \
+     -f linkerd2/values.yaml -f linkerd2/ha-values.yaml \
+     -n linkerd --version 2.8.1
 
 ############### Install Nginx Ingress Controller using Helm 3
 echo "--- Creating namespace ${namespace}-nginx-ingress..."
