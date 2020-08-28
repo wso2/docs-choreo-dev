0dea3188aab3b7da8459565d7f6f94926d64303a
Inzamam <inzamam.15@cse.mrt.ac.lk>
2020-08-28 16:09:21 +0530

    Enable app linking test

--- integration-tests/ui/src/tests/app-linking-flow.ts
+++ integration-tests/ui/src/tests/app-linking-flow.ts
@@ -18,33 +18,33 @@ fixture("App linking")
     });
 
 test("test app linking", async (t) => {
-    /*
-        await t.expect(Selector("#backdrop-loader").exists).notOk({ timeout: 20000 });
-        logger.info("Page loaded successfully");
-        await clearAppsIfExists(t);
-    
-        logger.info("Start connecting a running app : " + "linking-test-app");
-        await t
-            .click(screen.getAllByTestId("link-ballerina-app"))
-            .typeText(screen.getAllByPlaceholderText("App name"), "linking-test-app")
-            .click(screen.getByText("Generate Secret"));
-        await t.expect(screen.queryAllByTestId("copy-btn").exists).ok({ timeout: 60000 });
-        logger.info("Secret generated successfully");
-    
-        async function runBallerinaApp(secret: string) {
-            const secretConfig = 'secret=\\"' + secret + '\\"';
-            const { stdout, stderr } = await exec('sh src/utils/applinking_test/run_ballerina.sh ' + secretConfig);
-            console.log('stdout:', stdout);
-            console.log('stderr:', stderr);
-        }
-        const secret = (await screen.getByPlaceholderText("Application secret").value).toString();
-        console.log(secret);
-        await runBallerinaApp(secret);
-    
-        await t.expect(screen.findByText("Successfully Connected").exists).ok({ timeout: 120000 });
-        await t.click(screen.findByText("Observe"));
-        await t.expect(await getLocation()).contains("observe/", { timeout: 20000 });
-        logger.info("App Linking successful");*/
+
+    await t.expect(Selector("#backdrop-loader").exists).notOk({ timeout: 20000 });
+    logger.info("Page loaded successfully");
+    await clearAppsIfExists(t);
+
+    logger.info("Start connecting a running app : " + "linking-test-app");
+    await t
+        .click(screen.getAllByTestId("link-ballerina-app"))
+        .typeText(screen.getAllByPlaceholderText("App name"), "linking-test-app")
+        .click(screen.getByText("Generate Secret"));
+    await t.expect(screen.queryAllByTestId("copy-btn").exists).ok({ timeout: 60000 });
+    logger.info("Secret generated successfully");
+
+    async function runBallerinaApp(secret: string) {
+        const secretConfig = 'secret=\\"' + secret + '\\"';
+        const { stdout, stderr } = await exec('sh src/utils/applinking_test/run_ballerina.sh ' + secretConfig);
+        console.log('stdout:', stdout);
+        console.log('stderr:', stderr);
+    }
+    const secret = (await screen.getByPlaceholderText("Application secret").value).toString();
+    console.log(secret);
+    await runBallerinaApp(secret);
+
+    await t.expect(screen.findByText("Successfully Connected").exists).ok({ timeout: 120000 });
+    await t.click(screen.findByText("Observe"));
+    await t.expect(await getLocation()).contains("observe/", { timeout: 20000 });
+    logger.info("App Linking successful");
 });
 
 
--- integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
+++ integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 cd src/utils/applinking_test &&
-docker container run --rm -it -v "$(pwd):/home/ballerina" -u "$(id -u):$(id -g)" -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview3 ballerina build --observability-included test &&
-rm -f ballerina.conf && 
-cp ballerina.template.conf ballerina.conf && 
+docker container run --rm -v $(pwd):/home/ballerina -u $(id -u):$(id -g) -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview3 ballerina build --observability-included test &&
+rm -f ballerina.conf .choreoproject && 
+if [ "$env" = "dev" ]; then   
+    cp ballerina.dev.template.conf ballerina.conf
+elif [ "$env" = "stage" ]; then
+    cp ballerina.stage.template.conf ballerina.conf
+elif [ "$env" = "prod" ]; then
+    cp ballerina.prod.template.conf ballerina.conf
+fi &&
 echo "[b7a.observability.choreo.application]" >> ballerina.conf && 
 echo "$1" >> ballerina.conf && 
 java -jar target/bin/test.jar
