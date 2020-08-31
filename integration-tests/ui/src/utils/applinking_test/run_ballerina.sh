1c6010a78d4afd93b6ab7173475059260fd1e067
Inzamam <inzamam.15@cse.mrt.ac.lk>
2020-08-31 11:59:45 +0530

    Change environment names in shell script

--- integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
+++ integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
@@ -2,11 +2,11 @@
 cd src/utils/applinking_test &&
 docker container run --rm -v "$(pwd)":/home/ballerina -u "$(id -u)":"$(id -g)" -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview3 ballerina build --observability-included test &&
 rm -f ballerina.conf .choreoproject &&
-if [ "$CHOREO_ENV" = "dev" ]; then   
+if [ "$CHOREO_ENV" = "DEV" ]; then   
     cp ballerina.dev.template.conf ballerina.conf
-elif [ "$CHOREO_ENV" = "stage" ]; then
+elif [ "$CHOREO_ENV" = "STAGE" ]; then
     cp ballerina.stage.template.conf ballerina.conf
-elif [ "$CHOREO_ENV" = "prod" ]; then
+elif [ "$CHOREO_ENV" = "PROD" ]; then
     cp ballerina.prod.template.conf ballerina.conf
 fi &&
 echo "[b7a.observability.choreo.application]" >> ballerina.conf && 
