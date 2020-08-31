591e2efce46a4fd9b1ebc30144e3cf9039f57616
Inzamam <inzamam.15@cse.mrt.ac.lk>
2020-08-31 11:50:50 +0530

    Fix shell linter issue

--- integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
+++ integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd src/utils/applinking_test &&
-docker container run --rm -v $(pwd):/home/ballerina -u $(id -u):$(id -g) -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview3 ballerina build --observability-included test &&
-rm -f ballerina.conf .choreoproject && 
-if [ "$env" = "dev" ]; then   
+docker container run --rm -v "$(pwd)":/home/ballerina -u "$(id -u)":"$(id -g)" -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview3 ballerina build --observability-included test &&
+rm -f ballerina.conf .choreoproject &&
+if [ "$CHOREO_ENV" = "dev" ]; then   
     cp ballerina.dev.template.conf ballerina.conf
-elif [ "$env" = "stage" ]; then
+elif [ "$CHOREO_ENV" = "stage" ]; then
     cp ballerina.stage.template.conf ballerina.conf
-elif [ "$env" = "prod" ]; then
+elif [ "$CHOREO_ENV" = "prod" ]; then
     cp ballerina.prod.template.conf ballerina.conf
 fi &&
 echo "[b7a.observability.choreo.application]" >> ballerina.conf && 
