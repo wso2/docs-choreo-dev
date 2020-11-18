4183900ef26a21f87b13fb90394ca5de51d53636
Kavith Lokuhewage <kaviththiranga@gmail.com>
2020-11-18 22:09:48 +0530

    Fix text inserting to exp editor

--- integration-tests/ui/src/utils/choreo-utils.ts
+++ integration-tests/ui/src/utils/choreo-utils.ts
@@ -120,11 +120,14 @@ export const createProperty = async (t: TestController, name: string, expression
       { speed: 0.5 }
     )
     .click(screen.getByText("Define Expression"), { speed: 0.5 })
+    .click(Selector('.exp-editor .monaco-editor .view-line').nth(0))
+    .wait(3000)
     .typeText(
-      screen.getByPlaceholderText('eg: "Hello world"'),
+      Selector('.exp-editor .monaco-editor .inputarea').nth(0),
       expression,
       { speed: 0.5 }
     )
+    .wait(1000)
     .click(screen.getByText("Save"))
     .expect(screen.findByTestId("diagram-loader").exists).ok({ timeout: WAIT_TIME_SHORT })
     .expect(screen.findByTestId("diagram-loader").exists).notOk({ timeout: WAIT_TIME_MEDIUM });
@@ -140,11 +143,14 @@ export const createRespond = async (t: TestController, expression: string) => {
     .click(screen.getByTestId("stop-oval"), { speed: 0.5 })
     .hover(screen.getByText("Respond"), { speed: 0.5 })
     .click(screen.getByTestId("addrespond"), { speed: 0.5 })
+    .click(Selector('.exp-editor .monaco-editor .view-line').nth(0))
+    .wait(3000)
     .typeText(
-      screen.getByPlaceholderText('eg: "Executed successfully!"'),
-      "res",
+      Selector('.exp-editor .monaco-editor .inputarea').nth(0),
+      expression,
       { speed: 0.5 }
     )
+    .wait(1000)
     .click(screen.getByText("Save"))
     .expect(screen.findByTestId("diagram-loader").exists).ok({ timeout: WAIT_TIME_SHORT })
     .expect(screen.findByTestId("diagram-loader").exists).notOk({ timeout: WAIT_TIME_LONG });
