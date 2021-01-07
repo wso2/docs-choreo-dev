33813a94cf8d1fb820d690640c748928c5d0dae9
lakshankarunathilake <manualsplash@gmail.com>
2021-01-07 11:59:15 +0530

    Migrate linking app from slp3 to slp8

--- integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
+++ integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 cd src/utils/applinking_test &&
-docker container run --rm -v "$(pwd)":/home/ballerina -u "$(id -u)":"$(id -g)" -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview3 ballerina build --observability-included test &&
+docker container run --rm -v "$(pwd)":/home/ballerina -u "$(id -u)":"$(id -g)" -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview8 ballerina build --observability-included &&
 rm -f ballerina.conf .choreoproject &&
-if [ "$CHOREO_ENV" = "DEV" ]; then   
+if [ -z "$CHOREO_ENV" ]
+then
+  echo "\$CHOREO_ENV variable is empty !"  >&2
+else
+  if [ "$CHOREO_ENV" = "DEV" ]; then
     cp ballerina.dev.template.conf ballerina.conf
-elif [ "$CHOREO_ENV" = "STAGE" ]; then
+  elif [ "$CHOREO_ENV" = "STAGE" ]; then
     cp ballerina.stage.template.conf ballerina.conf
-elif [ "$CHOREO_ENV" = "PROD" ]; then
+  elif [ "$CHOREO_ENV" = "PROD" ]; then
     cp ballerina.prod.template.conf ballerina.conf
+  fi
 fi &&
 echo "[b7a.observability.choreo.application]" >> ballerina.conf && 
 echo "$1" >> ballerina.conf && 
-java -jar target/bin/test.jar
+java -jar target/bin/applinking_test.jar
