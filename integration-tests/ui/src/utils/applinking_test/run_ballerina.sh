34b391071a470843f5a79b20ab43a515fab3dc3f
lakshankarunathilake <manualsplash@gmail.com>
2021-01-07 14:03:21 +0530

    Update env missing log

--- integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
+++ integration-tests/ui/src/utils/applinking_test/run_ballerina.sh
@@ -2,17 +2,14 @@
 cd src/utils/applinking_test &&
 docker container run --rm -v "$(pwd)":/home/ballerina -u "$(id -u)":"$(id -g)" -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-preview8 ballerina build --observability-included &&
 rm -f ballerina.conf .choreoproject &&
-if [ -z "$CHOREO_ENV" ]
-then
-  echo "\$CHOREO_ENV variable is empty !"  >&2
+if [ "$CHOREO_ENV" = "DEV" ]; then
+  cp ballerina.dev.template.conf ballerina.conf
+elif [ "$CHOREO_ENV" = "STAGE" ]; then
+  cp ballerina.stage.template.conf ballerina.conf
+elif [ "$CHOREO_ENV" = "PROD" ]; then
+  cp ballerina.prod.template.conf ballerina.conf
 else
-  if [ "$CHOREO_ENV" = "DEV" ]; then
-    cp ballerina.dev.template.conf ballerina.conf
-  elif [ "$CHOREO_ENV" = "STAGE" ]; then
-    cp ballerina.stage.template.conf ballerina.conf
-  elif [ "$CHOREO_ENV" = "PROD" ]; then
-    cp ballerina.prod.template.conf ballerina.conf
-  fi
+  echo "\$CHOREO_DEV env variable should be one of 'DEV', 'TAGE', 'PROD' !"  >&2
 fi &&
 echo "[b7a.observability.choreo.application]" >> ballerina.conf && 
 echo "$1" >> ballerina.conf && 
