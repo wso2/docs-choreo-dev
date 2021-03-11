df8568f7c37e0b4e87986484a63dd7c2a74cc73b
DMadhuranga <danushkam@wso2.com>
2021-03-11 17:38:06 +0530

    Split periodic tests

--- integration-tests/ui/src/clean/clean-this-run.ts
+++ integration-tests/ui/src/clean/clean-this-run.ts
@@ -42,7 +42,7 @@ fixture.meta({ 'apim': "true" })("Cleaning up")
 
   });
 
-test.meta({'stable': "true", 'unstable': "true", 'periodic': "true"})("Delete Apps and APIs that are old or created by this run", async (t) => {
+test.meta({'stable': "true", 'unstable': "true", 'periodic': "true"})("Delete Apps that are old or created by this run", async (t) => {
   
   let appsToBeDeleted = [];
   while (true) {
@@ -70,7 +70,9 @@ test.meta({'stable': "true", 'unstable': "true", 'periodic': "true"})("Delete Ap
   for (let index = 0; index < appsToBeDeleted.length; index++) {
     await deleteApp(t, appsToBeDeleted[index], false);
   }
-  
+});
+
+test.meta({'unstable': "true"})("Delete APIs that are old or created by this run", async (t) => {
   await goToApiListView(t);
 
   let apisToBeDeleted = [];
