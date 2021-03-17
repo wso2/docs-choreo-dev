b72ce1081632995f6da060f200ceb4d7ab807aa0
Nisrin <nisrin@wso2.com>
2021-03-17 10:46:28 +0530

    Remove dist folder as it is not necessary

--- src/.vuepress/dist/assets/js/24.24908ada.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{487:function(n,w,o){}}]);
--- src/.vuepress/dist/assets/js/27.51381ecb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{490:function(n,w,o){}}]);
