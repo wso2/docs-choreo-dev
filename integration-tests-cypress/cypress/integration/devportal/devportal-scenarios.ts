75ac4b0e607dc2793f41eafec2e54144ce37c4ce
ketharan <sketharan1996.15@cse.mrt.ac.lk>
2021-05-06 03:12:05 +0530

    Fixes a test issue and updates scripts

--- integration-tests-cypress/cypress/integration/devportal/devportal-scenarios.ts
+++ integration-tests-cypress/cypress/integration/devportal/devportal-scenarios.ts
@@ -4,7 +4,7 @@ import { generateAppName } from "../../support/common/choreo-utils";
 
 describe('Devportal', () => {
     const appName = generateAppName('-e2etest');
-    const apiName = 'e2eTestApiDv';
+    const apiName = 'e2eTestApiDv1';
 
     beforeEach(() => {
         cy.devportalLogin();
