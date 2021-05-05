5ad0b2405af8d8c2ce821de85d33895d9cc4cceb
madushajg <madushajg@gmail.com>
2021-05-05 20:24:27 +0530

    [e2e] Add test cases for logs view

--- integration-tests-cypress/cypress/integration/console/observability-test-flow.ts
+++ integration-tests-cypress/cypress/integration/console/observability-test-flow.ts
@@ -17,6 +17,7 @@ import { generateAppName } from '../../support/common/choreo-utils';
 describe('Observability tests', () => {
     let savedCookies
     let appName: string
+    let obsUrl: string
 
     before(() => {
         cy.log("Login into Choreo using Google")
@@ -24,31 +25,7 @@ describe('Observability tests', () => {
         cy.getCookies().then((cookies) => {
             savedCookies = cookies
         })
-    })
-    
-    beforeEach(() => {
-        savedCookies.map((cookie) => {
-        cy.setCookie(cookie.name, cookie.value, {
-            domain: cookie.domain,
-            expiry: cookie.expires,
-            httpOnly: cookie.httpOnly,
-            path: cookie.path,
-            secure: cookie.secure
-        })
 
-        Cypress.Cookies.defaults({
-            preserve: cookie.name
-        })
-        })    
-    });
-
-    after(() => {
-        cy.goBacktoAppsList();
-        cy.deleteApp(appName, true);
-        cy.userLogout()
-    })
-
-    it('navigate to sample service observability view', () => {
         appName = generateAppName("app");
         cy.log('app name: ', appName);
         cy.createNewApp(appName);
@@ -57,23 +34,64 @@ describe('Observability tests', () => {
         cy.get('[data-testid="observe"]').should('be.visible');
         cy.get('[data-testid="observe"]').click();
 
-        var obsUrl = '';
         cy.window().then((win) => {
             cy.stub(win, 'open').as('windowOpen').callsFake(url => {
-                obsUrl = url;
+                obsUrl = url.replace('http://', 'https://');
             });
         })
 
         cy.contains('button', 'Sample Service').click();
         cy.get('@windowOpen').should('be.called');
-        cy.wait(4000).then(() => {
-            cy.visit(obsUrl.replace('http://', 'https://'))
+        cy.wait(1000).then(() => {
+            cy.visit(obsUrl)
         })
+    })
+    
+    beforeEach(() => {
+        savedCookies.map((cookie) => {
+            cy.setCookie(cookie.name, cookie.value, {
+                domain: cookie.domain,
+                expiry: cookie.expires,
+                httpOnly: cookie.httpOnly,
+                path: cookie.path,
+                secure: cookie.secure
+            })
+
+            Cypress.Cookies.defaults({
+                preserve: cookie.name
+            })
+        })
+
+        cy.visit(obsUrl)
+    });
+
+    after(() => {
+        cy.goBacktoAppsList();
+        cy.deleteApp(appName, true);
+        cy.userLogout()
     })
 
     it('test logs view', () => {
         cy.get('[data-testid="panel-Logs-btn"]').should('be.visible');
         cy.get('[data-testid="panel-Logs-btn"]').click();
+        cy.get('[data-testid="log-panel"]').should('be.visible');
+
+        cy.log('asseting mandatory log entry without any filter');
+        cy.get('span').contains('error while connecting to the hr-service');
+
+        cy.log('asseting mandatory log entry by providing a search phrace');
+        cy.get('[id="log-search"]').type('employee information not found in the hr-service');
+        cy.contains('button', 'Apply').click();
+        cy.get('[data-testid="log-panel"]').should('be.visible');
+        cy.get('span').contains('employee information not found in the hr-service');
+        cy.get('span').contains('error while connecting to the hr-service').should('not.exist');
+
+        cy.log('asseting log download');
+        cy.get('[id="log-search"]').click().clear().type("ballerina");
+        cy.contains('button', 'Apply').click();
+        cy.get('[data-testid="log-panel"]').should('be.visible');
+        cy.contains('button', 'Download').click();
+        cy.readFile('./cypress/downloads/employee-service-logs.txt').should('contain', '[INFO] [ballerina/http] started HTTP/WS listener 0.0.0.0:8090');
     })
 
 })
