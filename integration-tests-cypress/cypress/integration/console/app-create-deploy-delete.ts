c98ddcbaa312bedf1c3b2d9e7ee9b20ce5294c42
AathmanT <aathmant@gmail.com>
2021-05-10 12:03:58 +0530

    Changed login and updated app creation for new UI change

--- integration-tests-cypress/cypress/integration/console/app-create-deploy-delete.ts
+++ integration-tests-cypress/cypress/integration/console/app-create-deploy-delete.ts
@@ -1,4 +1,17 @@
-import { generateAppName } from '../../support/choreo-utils';
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
+
+import { generateAppName } from '../../support/common/choreo-utils';
 
 /// <reference types="cypress" />
 
@@ -7,8 +20,8 @@ describe('Application test run and deployment', () => {
     let appName: string
 
     before(() => {
-        cy.log("Logging into Choreo using google")
-        cy.userLoginWithGmail()
+        cy.log("Login into Choreo using Google")
+        cy.consoleUserLogin()
         cy.getCookies().then((cookies) => {
             savedCookies = cookies
         })
@@ -19,26 +32,14 @@ describe('Application test run and deployment', () => {
     })
 
     beforeEach(() => {
-        savedCookies.map((cookie) => {
-            cy.setCookie(cookie.name, cookie.value, {
-                domain: cookie.domain,
-                expiry: cookie.expires,
-                httpOnly: cookie.httpOnly,
-                path: cookie.path,
-                secure: cookie.secure
-            })
-
-            Cypress.Cookies.defaults({
-                preserve: cookie.name
-            })
-        })
-
+        cy.preserveCookiesForTest(savedCookies);
         appName = generateAppName("app");
         cy.log('app name: ', appName);
-        cy.createNewApp(appName);
+        cy.createNewApp("service", appName);
         cy.url().should('include', 'app/' + appName + '/develop');
-        cy.selectTrigger("API", "hello");
-        cy.createProperty("var", "res", '"hello world"');
+        cy.configureResource("hello");
+        cy.selectManualTriggerOptions("Statements", "addVariable");
+        cy.createVariableProperty("var", "res", '"hello world"');
         cy.createRespond("res");
     })
 
