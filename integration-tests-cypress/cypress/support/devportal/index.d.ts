69505c4af102193ff6b30ab7054f27db1012e59e
Shanaka M <shanakama@wso2.com>
2021-05-14 14:14:52 +0530

    Add devportal test suit separation

--- integration-tests-cypress/cypress/support/devportal/index.d.ts
+++ integration-tests-cypress/cypress/support/devportal/index.d.ts
@@ -16,6 +16,7 @@
 declare namespace Cypress {
     interface Chainable {
         devportalLogin(): Chainable<Element>
+        devportalLogout(): Chainable<Element>
         navigateToOverviewInDevportal(apiName: string): Chainable<Element>
         findByText(apiaName: string): Chainable<Element>
         findByRole(apiaName: string, object: any): Chainable<Element>
