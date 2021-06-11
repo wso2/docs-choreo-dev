126fe99732cb642b3a6373073004f8e98548b1cb
hareendran7 <hareendran.1st@gmail.com>
2021-06-11 11:26:48 +0530

    Remove har support from integration tests

--- e2e-tests-cypress/cypress/e2e/console/1-integrations/1.3-schedule-trigger.ts
+++ e2e-tests-cypress/cypress/e2e/console/1-integrations/1.3-schedule-trigger.ts
@@ -13,7 +13,6 @@
 
 import { generateAppName } from '../../../support/common/utils';
 import { INTEGRATIONS_TEXT, NO_OF_RETRIES } from '../../../support/common/constants';
-import { recordHar, saveHar } from "../../../support/common/harGenerator";
 
 /// <reference types="cypress" />
 
@@ -22,7 +21,6 @@ describe('Schedule trigger test run and deployment', () => {
     let appName: string
 
     before(() => {
-        recordHar();
         cy.consoleUserLogin()
         cy.getCookies().then((cookies) => {
             savedCookies = cookies
@@ -31,7 +29,6 @@ describe('Schedule trigger test run and deployment', () => {
 
     after(() => {
         cy.userLogout();
-        saveHar();
     })
 
     beforeEach(() => {
