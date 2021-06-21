d97f5488bfbc46acb79e4eebfa970787e3fdeb60
Ranika <ranikamadurawe@gmail.com>
2021-06-21 16:43:53 +0530

    Install linkerd viz extension

--- setup/azure/azure-setup.sh
+++ setup/azure/azure-setup.sh
@@ -124,6 +124,8 @@ helm upgrade --install linkerd2 --wait \
   --set installNamespace=false --set linkerdVersion=stable-2.10.0 \
   -n linkerd --version 2.10.0
 
+echo "--- Installing linkerd viz extension... "
+helm install linkerd-viz linkerd/linkerd-viz
 
 ############### Install Nginx Ingress Controller using Helm 3
 echo "--- Creating namespace ${namespace}-nginx-ingress..."
