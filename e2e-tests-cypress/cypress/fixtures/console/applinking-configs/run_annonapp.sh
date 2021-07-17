13fedd957741a83249368fef21c1f94c04b10ffe
shairam <shaikrish27@gmail.com>
2021-07-17 08:21:57 +0530

    Use prebuilt jar for anonymous app linking test

--- e2e-tests-cypress/cypress/fixtures/console/applinking-configs/run_annonapp.sh
+++ e2e-tests-cypress/cypress/fixtures/console/applinking-configs/run_annonapp.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd cypress/fixtures/console/applinking-configs &&
-docker container run --rm -v "$(pwd)":/home/ballerina -u "$(id -u)":"$(id -g)" -e JAVA_OPTS="-Duser.home=/home/ballerina" ballerina/ballerina:swan-lake-alpha5 bal build --observability-included &&
 rm -f Config.toml .choreoproject Dependencies.toml &&
 sed 's|<REPORTER_HOST_NAME>|'"$REPORTER_HOST_NAME"'|' Config.template.toml > Config.toml &&
-java -jar target/bin/applinking_test.jar
+java -jar applinking_test.jar
