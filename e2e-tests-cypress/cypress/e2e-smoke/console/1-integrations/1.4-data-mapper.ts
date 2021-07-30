8bfadb23848a53fc07917edbf837ab997f6e251d
rasika2012 <rasikam@wso2.com>
2021-07-30 17:14:16 +0530

    Fix for Side panel

--- e2e-tests-cypress/cypress/e2e-smoke/console/1-integrations/1.4-data-mapper.ts
+++ e2e-tests-cypress/cypress/e2e-smoke/console/1-integrations/1.4-data-mapper.ts
@@ -33,6 +33,7 @@ describe("Data Mapper", () => {
         cy.createNewApp(INTEGRATIONS_TEXT, appName);
         cy.url().should("include", "app/" + appName + "/develop");
         cy.selectTrigger("Manual");
+        cy.get('[data-testid="vertical-close-btn"]').click();
 
         cy.selectManualTriggerOptions("Statements", "addVariable");
         cy.createVariableProperty('string', 'fName', '"John"');
@@ -43,13 +44,11 @@ describe("Data Mapper", () => {
         cy.selectManualTriggerOptions("Statements", "addVariable");
         cy.createVariableProperty('int', 'age', '30');
 
-        cy.get('[data-testid="product-tour-code-view"]').click();
 
         cy.selectManualTriggerOptions("Statements", "addDataMapping");
  
         cy.get('[data-testid="datamapper-variable-name"]').find('input').first()
             .click({ force: true }).clear().type('emp');
-        cy.get('[data-testid=product-tour-code-view]').click();
         cy.get('[data-testid="vertical-close-btn"]').click();
         cy.get('[data-testid = "Select Typestring"]').click();
         cy.contains('json').click({force: true});
@@ -113,7 +112,7 @@ describe("Data Mapper", () => {
         });
         cy.url().should("include", "app/" + appName + "/develop");
         cy.waitTillWorkSpace();
-
+        cy.get('[data-testid="vertical-close-btn"]').click();
         
         cy.log('Editing DataMapper');
         cy.get('[data-testid="data-processor-block"]').eq(3).trigger('mouseover').within(() => {
@@ -125,8 +124,6 @@ describe("Data Mapper", () => {
         cy.get('[data-testid="datamapper-save-btn"]').click();
         cy.waitTillWorkSpace();
 
-        cy.get('[data-testid=product-tour-code-view]').click();
-        cy.get('[data-testid="vertical-close-btn"]').click();
         cy.get('[data-testid=datamapper-diagram-switch]').click();
         cy.log("DataMapper edited successfully!");
 
@@ -134,8 +131,6 @@ describe("Data Mapper", () => {
         cy.log('Deleting DataMapper');
         cy.get('g:nth-child(9) #DeleteIcon').click({force:true});
         cy.get('[data-testid="delete-logic-block-btn"] > .MuiButton-label').click({force:true});
-        cy.get('[data-testid=product-tour-code-view]').click();
-        cy.get('[data-testid="vertical-close-btn"]').click();
         cy.log("DataMapper deleted successfully!");
            
 
