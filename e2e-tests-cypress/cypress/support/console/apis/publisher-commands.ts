3fa373aeddfb614882fcdf75dff9b300caf6be9b
amali <maamalilakshika@gmail.com>
2021-08-14 09:52:01 +0530

    Improve 3.3 test case

--- e2e-tests-cypress/cypress/support/console/apis/publisher-commands.ts
+++ e2e-tests-cypress/cypress/support/console/apis/publisher-commands.ts
@@ -152,6 +152,7 @@ Cypress.Commands.add('deployInitialRevision', () => {
     cy.get('.MuiDialogContent-root').within(() => {
         cy.get('button').contains('Deploy').click();
     });
+    cy.wait(3000);
     cy.get('[data-testid=deployment-loader]', { timeout: DEPLOYMENT_TIME_OUT }).should('not.exist');
 });
 
