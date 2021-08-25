e0a5083ceecd5d799d510f27298529d44490b13d
uvindra <uvindra_dj@yahoo.com>
2021-08-25 15:39:47 +0530

    Improvements to test data cleanup
    1. Improved tests data cleanup functionality.
    2. Improved ability to debug test data cleanup related issues.
    3. Added a utility script for cleaning data adhoc.

--- e2e-tests-cypress/cypress/support/console/common/index.d.ts
+++ e2e-tests-cypress/cypress/support/console/common/index.d.ts
@@ -55,6 +55,7 @@ declare namespace Cypress {
         undeployApp(type: string, name: string, strict: boolean): Chainable<Element>
         deleteApp(type: string, name: string, strict: boolean): Chainable<Element>
         cleanupApp(name: string, orgHandle: string): Chainable<Element>
+        cleanupApi(apiId: string, orgId: string, token: string): Chainable<Element>
         createRespond(expression: string, skipSmallPlus?: boolean): Chainable<Element>
         callExternalEndpoint(URL: string, attempts: number, expectedRes: string): Chainable<Element>
         switchToDeployView(appName: string): Chainable<Element>
