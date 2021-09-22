b958494481f59489255045c2118082704e218413
Shehanir <shehanir@wso2.com>
2021-09-22 10:43:36 +0530

    updating mssql script of choreo_subscription_db with choreo_billing tables

--- databases/scripts/choreo_subscriptions_db_mssql.sql
+++ databases/scripts/choreo_subscriptions_db_mssql.sql
@@ -63,6 +63,62 @@ BEGIN
 END
 GO
 
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='billing_tier' and xtype='U')
+BEGIN
+    CREATE TABLE billing_tier (
+        id VARCHAR(128) NOT NULL,
+        choreo_tier_id VARCHAR(128) NOT NULL,
+        product_id VARCHAR(128) NOT NULL,
+        price_id VARCHAR(128) NOT NULL,
+        currency VARCHAR(128) NOT NULL,
+        recurring_interval VARCHAR(128) NOT NULL,
+        UNIQUE (product_id),
+        UNIQUE (price_id),
+        UNIQUE (choreo_tier_id),
+        PRIMARY KEY (choreo_tier_id, product_id)
+    );
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='billing_subscription' and xtype='U')
+BEGIN
+    CREATE TABLE billing_subscription (
+        id VARCHAR(128) NOT NULL,
+        choreo_subscription_id VARCHAR(128) NOT NULL,
+        stripe_subscription_id VARCHAR(128) NOT NULL,
+        stripe_subscription_item_id VARCHAR(128) NOT NULL,
+        UNIQUE (choreo_subscription_id),
+        PRIMARY KEY (choreo_subscription_id, stripe_subscription_id)
+    );
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='billing_account' and xtype='U')
+BEGIN
+    CREATE TABLE billing_account (
+        id VARCHAR(128) NOT NULL,
+        org_id VARCHAR(128) NOT NULL,
+        customer_id VARCHAR(128) NOT NULL,
+        UNIQUE (org_id),
+        UNIQUE (customer_id),
+        PRIMARY KEY (org_id, customer_id)
+    );
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='billing_history' and xtype='U')
+BEGIN
+    CREATE TABLE billing_history (
+        id VARCHAR(128) NOT NULL,
+        subscribed_user VARCHAR(256) NOT NULL,
+        customer_id VARCHAR(128) NOT NULL,
+        timestamp BIGINT DEFAULT DATEDIFF_BIG(MILLISECOND,'1970-01-01 00:00:00.000', SYSUTCDATETIME()),
+        operation VARCHAR(128) NOT NULL,
+        PRIMARY KEY (id)
+    );
+END
+GO
+
 -- Initial data for subscriptions
 -- Relevant issue: https://github.com/wso2-enterprise/choreo/issues/7627
 
@@ -96,4 +152,24 @@ INSERT INTO quota (tier_id,attribute_name,threshold) VALUES
 	 (N'01ebea43-be76-1d7a-b410-2d1b873c57af',N'api_quota',1000000),
 	 (N'01ebea43-be76-1d7a-b410-2d1b873c57af',N'remote_app_quota',1000000),
 	 (N'01ebea43-be76-1d7a-b410-2d1b873c57af',N'step_quota',1000000);
+GO
+
+INSERT INTO choreo_subscriptions_db.dbo.billing_tier (id,choreo_tier_id,product_id,price_id,currency,recurring_interval) VALUES
+	 (N'01ec1491-3eff-1aec-b511-2eec6e3c92d2',N'01ebea3a-7735-10be-b3c0-ba95f991e877',N'prod_K8pD0xG5AqXUzG',N'price_1JY0lgEeYOVsvOhWyxCaQmNy',N'USD',N'month'),
+	 (N'01ec1491-316e-1c84-9195-5bbb347a8a0b',N'01ebea43-be76-1d7a-b410-2d1b873c57af',N'prod_K7pD0xG5AqXUzG',N'price_2JY0lgEeYOVsvOhWyxCaQmNy',N'USD',N'month');
+GO
+
+INSERT INTO choreo_subscriptions_db.dbo.billing_subscription (id,choreo_subscription_id,stripe_subscription_id,stripe_subscription_item_id) VALUES
+	 (N'01ec148a-c530-1e10-9d57-44bf012eb896',N'0000060f-569d-4394-b689-4624b9a31b5b',N'sub_KCPaTebN0yENzY',N'si_KCPa3qnZBTVqO3'),
+	 (N'01ec148a-c367-11d8-b08c-8c01a83304e7',N'00151c87-07c0-48b5-ad68-103b1f6f1a90',N'sub_KCPaTebN1yENzY',N'si_KCPa4qnZBTVqO3');
+GO
+
+INSERT INTO choreo_subscriptions_db.dbo.billing_account (id,org_id,customer_id) VALUES
+	 (N'01ec148a-c163-15e4-aaa7-a098f46fe579',N'0000',N'cus_KCPa1wMr4UJjDX'),
+	 (N'01ec148a-c008-1fe6-938c-04fd4734d900',N'1111',N'cus_KCPa1wMr3UJjDX');
+GO
+
+INSERT INTO choreo_subscriptions_db.dbo.billing_history (id,subscribed_user,customer_id,timestamp,operation) VALUES
+	 (N'01ec1491-6d7a-17c8-81df-0c658bb60511',N'Jhon Marcus',N'cus_KCPa1wMr4UJjDX',1627639797657,N'upgrade'),
+	 (N'01ec1490-6a98-1fce-a8b8-41b6ae8735a6',N'Pillip Json',N'cus_KCPa1wMr3UJjDX',1627639797657,N'downgrade');
 GO
