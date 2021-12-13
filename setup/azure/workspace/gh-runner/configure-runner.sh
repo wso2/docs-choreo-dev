7fc6c493e0ec75a3adc7a87a6d8e4e054f85bba7
hareendran7 <hareendran.1st@gmail.com>
2021-12-13 18:54:30 +0530

    Remove trailing whitespace

--- setup/azure/workspace/gh-runner/configure-runner.sh
+++ setup/azure/workspace/gh-runner/configure-runner.sh
@@ -7,7 +7,7 @@ kubectl create namespace actions-runner-system --dry-run=client -o yaml | kubect
 helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
 
 helm install actions-runner-controller actions-runner-controller/actions-runner-controller --namespace actions-runner-system --set=authSecret.create=true \
-    --set=authSecret.github_token="${GITHUB_TOKEN}" --set=githubWebhookServer.enabled=true --set=githubWebhookServer.ports[0].nodePort=33080 \ 
+    --set=authSecret.github_token="${GITHUB_TOKEN}" --set=githubWebhookServer.enabled=true --set=githubWebhookServer.ports[0].nodePort=33080 \
     --set=nodeSelector.execution-mode=ghrunner --set=tolerations[0].operator=Exists,tolerations[0].effect=NoSchedule,tolerations[0].key=ghrunner \
     --set=githubWebhookServer.nodeSelector.execution-mode=ghrunner \
     --set=githubWebhookServer.tolerations[0].operator=Exists,githubWebhookServer.tolerations[0].effect=NoSchedule,githubWebhookServer.tolerations[0].key=ghrunner
