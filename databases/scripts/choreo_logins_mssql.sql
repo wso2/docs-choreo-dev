a16c2997016922a4e944d9d861f3cef61cf2a46a
athiththan11 <code19111995@gmail.com>
2021-12-15 15:52:18 +0530

    Add choreo_delete_manager_db scripts

--- databases/scripts/choreo_logins_mssql.sql
+++ databases/scripts/choreo_logins_mssql.sql
@@ -96,6 +96,12 @@ IF NOT EXISTS
 CREATE LOGIN billing_db_user with password = N'xxxxxxxxxxxxx'
 GO
 
+IF NOT EXISTS
+	(SELECT name FROM master.sys.sql_logins
+	WHERE name = 'choreo_delete_manager_db_user')
+CREATE LOGIN choreo_delete_manager_db_user with password = N'xxxxxxxxxxxxx'
+GO
+
 --IF NOT EXISTS
 --	(SELECT name FROM master.sys.sql_logins
 --	WHERE name = 'choreo-aiexp-db-reader')
