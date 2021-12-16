26ab330127e253e42035b83cd34a09e65713a29e
Ranika <ranikamadurawe@gmail.com>
2021-12-16 15:25:28 +0530

    Fix linter issue

--- setup/azure/controlplane/create-ingress-tls-secrets.sh
+++ setup/azure/controlplane/create-ingress-tls-secrets.sh
@@ -4,13 +4,13 @@
 # TODO - Opt to generating these secrets using SecretProviderClasses When the CSI Driver supports syncing to K8S Secrets
 
 # Subscription Ingress Resource TLS Secrets
-kubectl create secret tls ${ENV}-choreo-subscriptions-tls \
-    --cert ${SUBSCRIPTION_INGRESS_TLS_CERT_FILE_PATH}  \
-    --key ${SUBSCRIPTION_INGRESS_TLS_KEY_FILE_PATH} \
-    --namespace ${SYSTEM_NAMESPACE}
+kubectl create secret tls "${ENV}"-choreo-subscriptions-tls \
+    --cert "${SUBSCRIPTION_INGRESS_TLS_CERT_FILE_PATH}"  \
+    --key "${SUBSCRIPTION_INGRESS_TLS_KEY_FILE_PATH}" \
+    --namespace "${SYSTEM_NAMESPACE}"
 
 # Internal Ingress Resource TLS Secrets
-kubectl create secret tls ${ENV}-choreo-controlplane-internal-tls \
-   --cert ${CONTROLPLANE_INTERNAL_INGRESS_TLS_CERT_FILE_PATH} \
-   --key ${CONTROLPLANE_INTERNAL_INGRESS_TLS_KEY_FILE_PATH} \
-   --namespace ${SYSTEM_NAMESPACE}
+kubectl create secret tls "${ENV}"-choreo-controlplane-internal-tls \
+   --cert "${CONTROLPLANE_INTERNAL_INGRESS_TLS_CERT_FILE_PATH}" \
+   --key "${CONTROLPLANE_INTERNAL_INGRESS_TLS_KEY_FILE_PATH}" \
+   --namespace "${SYSTEM_NAMESPACE}"
