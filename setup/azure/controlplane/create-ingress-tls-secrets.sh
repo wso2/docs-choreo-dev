16b82d0add41f300b5c3095064e3883b6882c5ed
Ranika <ranikamadurawe@gmail.com>
2021-12-22 13:41:07 +0530

    Update secret creation for private link resources

--- setup/azure/controlplane/create-ingress-tls-secrets.sh
+++ setup/azure/controlplane/create-ingress-tls-secrets.sh
@@ -10,7 +10,12 @@ kubectl create secret tls "${ENV}"-choreo-subscriptions-tls \
     --namespace "${SYSTEM_NAMESPACE}"
 
 # Internal Ingress Resource TLS Secrets
-kubectl create secret tls "${ENV}"-choreo-controlplane-internal-tls \
-   --cert "${CONTROLPLANE_INTERNAL_INGRESS_TLS_CERT_FILE_PATH}" \
-   --key "${CONTROLPLANE_INTERNAL_INGRESS_TLS_KEY_FILE_PATH}" \
+kubectl create secret tls "${ENV}"-choreo-controlplane-system-internal-tls \
+   --cert "${CONTROLPLANE_SYSTEM_INTERNAL_INGRESS_TLS_CERT_FILE_PATH}" \
+   --key "${CONTROLPLANE_SYSTEM_INTERNAL_INGRESS_TLS_KEY_FILE_PATH}" \
    --namespace "${SYSTEM_NAMESPACE}"
+
+kubectl create secret tls "${ENV}"-choreo-controlplane-apim-internal-tls \
+   --cert "${CONTROLPLANE_APIM_INTERNAL_INGRESS_TLS_CERT_FILE_PATH}" \
+   --key "${CONTROLPLANE_APIM_INTERNAL_INGRESS_TLS_KEY_FILE_PATH}" \
+   --namespace "${APIM_NAMESPACE}"
