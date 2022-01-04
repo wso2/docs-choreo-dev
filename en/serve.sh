273f4df88f20af4fa1b2b7f1903a72ae2acc79d2
Shani Ranasinghe <shanira14@gmail.com>
2022-01-04 15:24:44 +0530

    fix spelling of mkdocs.yaml and home page

--- en/serve.sh
+++ en/serve.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 set -e
 mdspell  -n -a --en-us docs/**/*.md -d dictionary/en_US-large
+mdspell  -n -a --en-us mkdocs.yml -d dictionary/en_US-large
+mdspell  -n -a --en-us theme/material/templates/home-page.html -d dictionary/en_US-large
 find docs/** -type f -name '*.md' ! -path 'docs/**/*' | xargs -L1 markdown-link-check 
 mkdocs serve
