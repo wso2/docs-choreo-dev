3a20148f993cb9a1006e79b8f2fc77d0def6d1e9
Ranika <ranikamadurawe@gmail.com>
2022-01-13 16:50:46 +0530

    Fix Linter errors

--- setup/azure/azure-setup.sh
+++ setup/azure/azure-setup.sh
@@ -128,10 +128,10 @@ helm upgrade --install linkerd2 --wait \
 echo "---  Installing Buoyant Cloud... "
 kubectl create -f buoyant-cloud/buoyant-setup.sh
 kubectl create secret generic buoyant-cloud-id -n buoyant-cloud \
-  --from-literal=id=${BUOYANT_CLOUD_AGENT_ID} \
-  --from-literal=key=${BUOYANT_CLOUD_AGENT_KEY} \
-  --from-literal=downloadKey=${BUOYANT_CLOUD_AGENT_DOWNLOAD_KEY} \
-  --from-literal=name=${BUOYANT_CLOUD_NAME}
+  --from-literal=id="${BUOYANT_CLOUD_AGENT_ID}" \
+  --from-literal=key="${BUOYANT_CLOUD_AGENT_KEY}" \
+  --from-literal=downloadKey="${BUOYANT_CLOUD_AGENT_DOWNLOAD_KEY}" \
+  --from-literal=name="${BUOYANT_CLOUD_NAME}"
 kubectl label secret buoyant-cloud-id -n buoyant-cloud app.kubernetes.io/part-of=buoyant-cloud
 
 ## TODO: Migrate to Buoyant Cloud for Linkerd Monitoring
