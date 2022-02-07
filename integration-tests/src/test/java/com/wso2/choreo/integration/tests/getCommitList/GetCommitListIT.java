3ef843bd865967e419ef85a72b1f033a299f605e
nir099 <a.m.s.nirmal.a@gmail.com>
2022-02-08 01:55:41 +0530

    Add get commit list test

---
+++ integration-tests/src/test/java/com/wso2/choreo/integration/tests/getCommitList/GetCommitListIT.java
@@ -0,0 +1,113 @@
+package com.wso2.choreo.integration.tests.getCommitList;
+
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+import static com.consol.citrus.validation.json.JsonMessageValidationContext.Builder.json;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.message.MessageType;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import com.wso2.choreo.integration.common.ChoreoOrganization;
+import com.wso2.choreo.integration.common.TokenHandler;
+import com.wso2.choreo.integration.common.choreoproject.ChoreoProject;
+import com.wso2.choreo.integration.common.choreoproject.RestApiChoreoComponent;
+import com.wso2.choreo.integration.common.choreoproject.RestApiChoreoComponentBuilder;
+import com.wso2.choreo.integration.common.exceptions.ComponentCreationException;
+import com.wso2.choreo.integration.common.exceptions.ComponentCreationStatusCheckException;
+import com.wso2.choreo.integration.common.exceptions.ComponentCreationTimeoutException;
+import com.wso2.choreo.integration.common.exceptions.ComponentRetrieveException;
+import com.wso2.choreo.integration.common.exceptions.ProjectCreationException;
+import com.wso2.choreo.integration.common.exceptions.TokenRetrievalException;
+import com.wso2.choreo.integration.config.Configuration;
+import com.wso2.choreo.integration.config.Constant;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+
+/**
+ * Connector publishing related tests
+ */
+public class GetCommitListIT extends TestNGCitrusSpringSupport {
+
+  private static String componentId;
+  private static String accessToken;
+
+  @Autowired
+  private HttpClient choreoTestClient;
+
+  @BeforeSuite
+  public void beforeSuite()
+      throws IOException, InterruptedException, ProjectCreationException,
+      ComponentCreationStatusCheckException,
+      ComponentCreationException, ComponentRetrieveException,
+      ComponentCreationTimeoutException,
+      TokenRetrievalException {
+    TokenHandler tokenHandler = new TokenHandler();
+    accessToken = Constant.BEARER_PREFIX.concat(tokenHandler.getTestToken());
+    ChoreoOrganization org = new ChoreoOrganization(Configuration.TEST_CHOREO_ORG_HANDLE,
+        String.valueOf(Configuration.TEST_CHOREO_ORG_ID), Configuration.TEST_CHOREO_ORG_UUID);
+
+    ChoreoProject project = org.createProject(accessToken);
+
+    RestApiChoreoComponentBuilder restApiComponentBuilder = new RestApiChoreoComponentBuilder(project, org);
+
+    RestApiChoreoComponent restApiComponent = (RestApiChoreoComponent) project
+        .createChoreoComponent(restApiComponentBuilder, accessToken);
+
+    componentId = restApiComponent.getId();
+  }
+
+  @Test
+  @CitrusTest
+  public void testGetCommitList() throws JsonProcessingException {
+    HashMap<String, String> gqlRequestPayload = new HashMap<>() {
+      {
+        put("query", "query {" +
+            "      commitHistory(componentId: \"" + componentId + "\") {" +
+            "          author {" +
+            "            name," +
+            "            date," +
+            "            email," +
+            "            avatarUrl" +
+            "          }," +
+            "        message" +
+            "        sha" +
+            "        isLatest" +
+            "    }" +
+            "  }");
+      }
+    };
+    ObjectMapper objectMapper = new ObjectMapper();
+    String requestBody = objectMapper.writeValueAsString(gqlRequestPayload);
+    $(http()
+        .client(choreoTestClient)
+        .send()
+        .post("/graphql")
+        .message()
+        .header(HttpHeaders.AUTHORIZATION, accessToken)
+        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .body(requestBody)
+        .accept(String.valueOf(MediaType.APPLICATION_JSON)));
+    $(http()
+        .client(choreoTestClient)
+        .receive()
+        .response(HttpStatus.OK)
+        .message()
+        .name("createComponent")
+        .type(MessageType.JSON)
+        .body(new ClassPathResource("templates/getCommitList/query_get_commit_list_success.json"))
+        .validate(json()
+            .ignore("$.data.commitHistory[0].author.date")
+            .ignore("$.data.commitHistory[0].author.avatarUrl")
+            .ignore("$.data.commitHistory[0].sha")));
+  }
+
+}
