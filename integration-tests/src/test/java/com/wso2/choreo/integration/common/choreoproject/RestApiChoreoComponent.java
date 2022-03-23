4555881d2a1157e1501c4d17f32b07a0bab2427a
lakshankarunathilake <manualsplash@gmail.com>
2022-03-23 13:54:55 +0530

    Add method to invoke rest api component

--- integration-tests/src/test/java/com/wso2/choreo/integration/common/choreoproject/RestApiChoreoComponent.java
+++ integration-tests/src/test/java/com/wso2/choreo/integration/common/choreoproject/RestApiChoreoComponent.java
@@ -1,8 +1,58 @@
 package com.wso2.choreo.integration.common.choreoproject;
 
+import com.wso2.choreo.integration.common.exceptions.*;
+import com.wso2.choreo.integration.config.Constant;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.TimeUnit;
+
 /**
  * A class to represent a Rest API Choreo component
  */
 public class RestApiChoreoComponent extends ChoreoComponent {
+    private static final HttpClient client = HttpClient.newHttpClient();
 
+    /**
+     * Invoke the application for a given number of iterations
+     *
+     * @param accessToken   OAuth token to invoke the Chorea backend
+     * @param componentType type of the component
+     * @param environment   environment of the deployment
+     * @param count         number of times application need to be called
+     */
+    public void invokeGetApplication(String accessToken, String componentType, String environment, int count) throws
+            ComponentInvokeInformationCheckException, NoLatestApiVersionFoundException, IOException,
+            InterruptedException, GenerateAPIKeyCheckException, ApiKeyNotFoundException, InvokeInformationNotFoundException, InvokeAPICheckException {
+        InvokeInformation invokeInformation = getInvokeInformation(accessToken, componentType, environment);
+        String requestURI = invokeInformation.getInvokeUrl()
+                .concat("/")
+                .concat("greeting")
+                .concat("?name=testUser");
+
+        System.out.println("invokeInf" + invokeInformation.getApiId());
+        // Escaping the quotations
+        String apiKey = getAPIKeyForInvoke(accessToken, invokeInformation.getApiId()).replace("\"","");
+        int iteration = 0;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(requestURI))
+                .GET()
+                .header("API-Key", apiKey)
+                .header(HttpHeaders.CONTENT_TYPE, Constant.APPLICATION_JSON)
+                .build();
+        while (iteration < count) {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = response.statusCode();
+            if (statusCode != HttpStatus.OK.value()) {
+                throw new InvokeAPICheckException(statusCode, response.body());
+            }
+            iteration++;
+        }
+
+    }
 }
