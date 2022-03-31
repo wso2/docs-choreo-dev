19523454554d33f2743dac7ce0c7298b3e7d68c1
lasith650 <lasith650@gmail.com>
2022-03-31 17:45:00 +0530

    Implement citrus tests for Alert API

--- integration-tests/src/test/java/com/wso2/choreo/integration/config/Constant.java
+++ integration-tests/src/test/java/com/wso2/choreo/integration/config/Constant.java
@@ -50,7 +50,8 @@ public final class Constant {
     public static long COMPONENT_DEPLOY_TIMEOUT = 120000;
 
     public static final String INSIGHTS_API_RESOURCE = "/insights/1.0.0/query-api";
-
+    public static final String INSIGHTS_TRAFFIC_ALERT_API_RESOURCE = "/insightsalert/1.0.0/trafficConfigs";
+    public static final String INSIGHTS_LATENCY_ALERT_API_RESOURCE = "/insightsalert/1.0.0/latencyConfigs";
     public enum displayType {
         restAPI,
         proxy,
