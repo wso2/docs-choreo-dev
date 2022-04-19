f4309299ef39ed1fdb188412fd7f5302291615c3
NamalSanjaya <rathnanalanda159@gmail.com>
2022-04-19 09:53:36 +0530

    Change table and columns names

--- databases/scripts/choreo_codeserver_db_mssql.sql
+++ databases/scripts/choreo_codeserver_db_mssql.sql
@@ -5,39 +5,39 @@ GO
 USE choreo_codeserver_db;
 GO
 
-IF NOT  EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[clusters]') AND TYPE IN (N'U'))
-CREATE TABLE [clusters](
+IF NOT  EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[cluster]') AND TYPE IN (N'U'))
+CREATE TABLE [cluster](
     [cluster_id] VARCHAR(50) NOT NULL,
     [region] VARCHAR(40) NOT NULL,
     [hostname] VARCHAR(50) NOT NULL UNIQUE,
     [total] INT NOT NULL,
-    [time_stamp] DATETIME DEFAULT GETUTCDATE(),
-    CONSTRAINT PK_clusters PRIMARY KEY( [cluster_id] )
+    [last_clean_timestamp] DATETIME DEFAULT GETUTCDATE(),
+    CONSTRAINT PK_cluster PRIMARY KEY( [cluster_id] )
 )
 GO
 
-IF NOT  EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[codeservers]') AND TYPE IN (N'U'))
-CREATE TABLE [codeservers]( 
+IF NOT  EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[codeserver]') AND TYPE IN (N'U'))
+CREATE TABLE [codeserver]( 
     [id] INT NOT NULL IDENTITY(1,1),
-    [userid] NVARCHAR(60) NOT NULL,
-    [orgid]  NVARCHAR(60) NOT NULL,
-    [compid] NVARCHAR(60) NOT NULL,
-    [projid] NVARCHAR(60) NOT NULL,
+    [user_id] NVARCHAR(60) NOT NULL,
+    [organization_id]  NVARCHAR(60) NOT NULL,
+    [component_id] NVARCHAR(60) NOT NULL,
+    [project_id] NVARCHAR(60) NOT NULL,
     [cluster_id] VARCHAR(50) NOT NULL,
-    CONSTRAINT PK_codeservers PRIMARY KEY ([id]),
-    CONSTRAINT UC_codeservers UNIQUE ([userid],[orgid],[compid],[projid]),
-    CONSTRAINT FK_clusters_codeservers FOREIGN KEY ([cluster_id]) REFERENCES [clusters]([cluster_id])
+    CONSTRAINT PK_codeserver PRIMARY KEY ([id]),
+    CONSTRAINT UC_codeserver UNIQUE ([user_id],[organization_id],[component_id],[project_id]),
+    CONSTRAINT FK_cluster_codeserver FOREIGN KEY ([cluster_id]) REFERENCES [cluster]([cluster_id])
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
 GO
 
 CREATE TRIGGER [TRG_deducte_codeserver_total] 
-ON [codeservers]
+ON [codeserver]
 AFTER DELETE
 AS BEGIN 
     SET NOCOUNT ON;
-    UPDATE [clusters]
+    UPDATE [cluster]
     SET [total] = [total] - 1
     WHERE [cluster_id] IN (SELECT [cluster_id] FROM deleted) AND [total] >= 1
 END
