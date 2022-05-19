61b9db8190fdd2f7bf524f0bbf70c4adfc2ae9d8
NamalSanjaya <rathnanalanda159@gmail.com>
2022-05-19 11:52:30 +0530

    Change coloum sizes

--- databases/scripts/choreo_codeserver_db_mssql.sql
+++ databases/scripts/choreo_codeserver_db_mssql.sql
@@ -7,7 +7,7 @@ GO
 
 IF NOT  EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[cluster]') AND TYPE IN (N'U'))
 CREATE TABLE [cluster](
-    [cluster_id] VARCHAR(50) NOT NULL,
+    [cluster_id] VARCHAR(40) NOT NULL,
     [region] VARCHAR(40) NOT NULL,
     [hostname] VARCHAR(253) NOT NULL UNIQUE,
     [codeserver_count] INT NOT NULL,
@@ -19,13 +19,13 @@ GO
 IF NOT  EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[codeserver]') AND TYPE IN (N'U'))
 CREATE TABLE [codeserver]( 
     [id] INT NOT NULL IDENTITY(1,1),
-    [user_id] NVARCHAR(60) NOT NULL,
-    [organization_id]  NVARCHAR(60) NOT NULL,
-    [component_id] NVARCHAR(60) NOT NULL,
-    [project_id] NVARCHAR(60) NOT NULL,
-    [cluster_id] VARCHAR(50) NOT NULL,
+    [user_idp_id] NVARCHAR(50) NOT NULL,
+    [organization_uuid]  NVARCHAR(50) NOT NULL,
+    [component_uuid] NVARCHAR(50) NOT NULL,
+    [project_uuid] NVARCHAR(50) NOT NULL,
+    [cluster_id] VARCHAR(40) NOT NULL,
     CONSTRAINT PK_codeserver PRIMARY KEY ([id]),
-    CONSTRAINT UC_codeserver UNIQUE ([user_id], [organization_id], [component_id], [project_id]),
+    CONSTRAINT UC_codeserver UNIQUE ([user_idp_id], [organization_uuid], [component_uuid], [project_uuid]),
     CONSTRAINT FK_cluster_codeserver FOREIGN KEY ([cluster_id]) REFERENCES [cluster]([cluster_id])
     ON DELETE CASCADE ON UPDATE CASCADE
 )
