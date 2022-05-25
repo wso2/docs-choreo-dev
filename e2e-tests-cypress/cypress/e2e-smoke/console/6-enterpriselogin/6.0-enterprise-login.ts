0abc0eda38af346eeddbcfbf0290414664a9761c
JanakaSandaruwan <janakasandaruwan1996@gmail.com>
2022-05-25 13:43:49 +0530

    Fix review comments for enterprise login

--- e2e-tests-cypress/cypress/e2e-smoke/console/6-enterpriselogin/6.0-enterprise-login.ts
+++ e2e-tests-cypress/cypress/e2e-smoke/console/6-enterpriselogin/6.0-enterprise-login.ts
@@ -1,7 +1,21 @@
+/*
+
+Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+This software is the property of WSO2 Inc. and its suppliers, if any.
+Dissemination of any information or reproduction of any material contained
+herein is strictly forbidden, unless permitted by WSO2 in accordance with
+the WSO2 Commercial License available at http://wso2.com/licenses.
+For specific language governing the permissions and limitations under
+this license, please see the license as well as any agreement you’ve
+entered into with WSO2 governing the purchase of this software and any
+associated services.
+*/
+
 /// <reference types="cypress" />
 import { ChoreoHomePage } from "../../../support/console/pages/home/home-page";
+import { LoginPage } from "../../../support/console/pages/login-page";
 
-describe("Enterprise Login", () => {
+describe("Enterprise Login using auth0Idp", () => {
 
     before(() => {
       cy.request(Cypress.env("auth0LogoutUrl"),{"client_id":Cypress.env("auth0ClientID"), "returnTo": Cypress.env("enterpriseLoginUrl")});
@@ -12,30 +26,7 @@ describe("Enterprise Login", () => {
     })
 
     it('Enterprise login to console', () => {
-        cy.visit(Cypress.env("enterpriseLoginUrl"))
-        cy.get('button[id="enterprise-sign-in"]').should("be.visible", { timeout: 180000 });
-
-        cy.get('button[id="enterprise-sign-in"]').click()
-
-        cy.get("#outlined-basic").type(Cypress.env("enterpriseIDPUsername"));
-        cy.contains('Continue').click();
-
-        cy.get('input[id="username"]').should("be.visible", { timeout: 180000 });
-        cy.get("#username").type(Cypress.env("enterpriseIDPUsername"));
-        cy.get("#password").type(Cypress.env("enterpriseIDPPassword"), { log: false });
-        cy.contains('Continue').click();
-
-
-        cy.get('[data-testid="header-user-profile-menu"]', {
-            timeout: 180000,
-          }).should("be.visible");
-
-        cy.window()
-          .its("sessionStorage")
-          .invoke("getItem", "sign_out_url")
-          .then((url) => {
-            Cypress.env("sign_out_url", url);
-        });
+        LoginPage.enterpriseLogin()
     })
 })
 
