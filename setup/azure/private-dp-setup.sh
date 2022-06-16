bde4b3f27a417a5cbfb8ac78e4c47b74e9676e5a
hirana <hirana@wso2.com@choreo-dev-common-bastion.dqepi1kp05tevhl0paz2icj3fc.cx.internal.cloudapp.net>
2022-06-16 18:39:46 +0000

    Fix AZ CLI commands

--- setup/azure/private-dp-setup.sh
+++ setup/azure/private-dp-setup.sh
@@ -125,8 +125,8 @@ helm upgrade --install reflector emberstack/reflector --namespace cert-manager -
 echo -e "\n --- Creating AKS view cluster role binding to AAD --- \n"
 cp conf/view-cluster-role-binding.yaml conf/view-cluster-role-binding.yaml.backup
 
-AKS_RESOURCE_ID=$(az aks show --name choreo-"${CUSTOMER_NAME}"-dataplane-"${ENV}" --resource-group choreo-tbl-dataplane-dev-aks-rg --query "id" --output tsv)
-AKS_READONLY_AD_GROUP_ID=$(az role assignment list --scope "${AKS_RESOURCE_ID}" --query "[?contains(principalName, 'choreo-${CUSTOMER_NAME}-aks-rbac-reader')].{principalId:principalId}" --output tsv)
+AKS_RESOURCE_ID=$(az aks show --name choreo-"${CUSTOMER_NAME}"-dataplane-"${ENV}" --resource-group choreo-"${CUSTOMER_NAME}"-dataplane-"${ENV}"-aks-rg --query "id" --output tsv)
+AKS_READONLY_AD_GROUP_ID=$(az role assignment list --scope "${AKS_RESOURCE_ID}" --query "[?contains(principalName, 'choreo-${CUSTOMER_NAME}-dataplane-${ENV}-aks-rbac-reader')].{principalId:principalId}" --output tsv)
 
 sed -i "s/AKS_READONLY_AD_GROUP_ID/${AKS_READONLY_AD_GROUP_ID}/g" conf/view-cluster-role-binding.yaml 
 kubectl apply -f conf/view-cluster-role-binding.yaml
