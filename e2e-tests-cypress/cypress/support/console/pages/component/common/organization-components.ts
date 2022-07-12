34b027aa81fea29a94b7c9b47766341f20cad00e
dasun <dasun@wso2.com>
2022-07-12 10:04:41 +0530

    3.3 related changes

--- e2e-tests-cypress/cypress/support/console/pages/component/common/organization-components.ts
+++ e2e-tests-cypress/cypress/support/console/pages/component/common/organization-components.ts
@@ -36,11 +36,13 @@ export class OrganizationComponent {
   }
 
   static verifyEmailIsNotDisplayed(email: string) {
-    cy.contains("td", email).should("not.exist");
+    cy.get(`td[value="${email}"]`).should('not.exist')
+    // cy.contains("td", email).should("not.be.visible")
   }
 
   static verifyEmailIsDisplayed(email: string) {
-    cy.contains("td", email).should("be.visible");
+    cy.get(`td[value="${email}"]`).should('exist')
+  //  cy.contains("td", email).should("be.visible");
   }
 
   static inviteMembers(email: string, ...roles) {
