33f2b5053200f21bd77dcfd7708f351619e675ca
udhanMti <udhan@wso2.com>
2022-07-15 18:21:20 +0530

    Update csi driver installation command

--- setup/azure/secret-store-csi-driver/configure-csi-secret-store.sh
+++ setup/azure/secret-store-csi-driver/configure-csi-secret-store.sh
@@ -7,4 +7,4 @@ kubectl create namespace csi-secret-store-driver --dry-run=client -o yaml | kube
 echo "--- Installing secret-store-csi-driver..."
 helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts
 helm repo update
-helm upgrade --install csi-secrets-store-provider-azure csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --namespace csi-secret-store-driver --version 1.0.0 --set secrets-store-csi-driver.linux.driver.resources.limits.memory=400Mi --set secrets-store-csi-driver.linux.driver.resources.requests.memory=200Mi
+helm upgrade --install csi-secrets-store-provider-azure csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --namespace csi-secret-store-driver --version 1.0.0 --set secrets-store-csi-driver.linux.driver.resources.limits.memory=400Mi --set secrets-store-csi-driver.linux.driver.resources.requests.memory=200Mi --set secrets-store-csi-driver.syncSecret.enabled=true
