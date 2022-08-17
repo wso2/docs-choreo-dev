c40c8f06c1f88a9d2c4bde6a9a555cbb37f7739f
Ranika <ranikamadurawe@gmail.com>
2022-08-17 09:10:02 +0530

    Fix linter issues

--- setup/azure/controlplane/superset/configure-superset.sh
+++ setup/azure/controlplane/superset/configure-superset.sh
@@ -2,8 +2,4 @@
 
 ################ Install CSI Secret Store Driver ########
 # Download Required secrets
-
-export SUPERSET_DB_PASS=$(az keyvault secret show --name postgresql-SUPERSET-DB-PASSWORD --vault-name ${CSI-KEYVAULT-NAME} --query value -o tsv)
-export SUPERSET_ADMIN_USER_PASS$(az keyvault secret show --name superset-ADMIN-PASSWORD --vault-name ${CSI-KEYVAULT-NAME} --query value -o tsv)
-
 bash ../../superset/install-superset.sh
--- setup/azure/superset/install-superset.sh
+++ setup/azure/superset/install-superset.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-############### Install Certmanager ####################
+SUPERSET_DB_PASS=$(az keyvault secret show --name postgresql-SUPERSET-DB-PASSWORD --vault-name "${CSI-KEYVAULT-NAME}" --query value -o tsv)
+SUPERSET_ADMIN_USER_PASS=$(az keyvault secret show --name superset-ADMIN-PASSWORD --vault-name "${CSI-KEYVAULT-NAME}" --query value -o tsv)
+
 kubectl create ns superset
 
 # Login to helm registry
-helm registry login choreocontrolplane.azurecr.io --username ${HELM_ACR_USERNAME}
+helm registry login choreocontrolplane.azurecr.io --username "${HELM_ACR_USERNAME}"
 
 # Pull Helm Chart
 helm pull oci://choreocontrolplane.azurecr.io/helm/superset --version 0.7.1
 
 # Install
 helm upgrade --install superset superset-0.7.1.tgz \
-    --set supersetNode.connections.db_pass=${SUPERSET_DB_PASS}
-    --set init.adminUser.password=${SUPERSET_ADMIN_USER_PASS}
+    --set supersetNode.connections.db_pass="${SUPERSET_DB_PASS}" \
+    --set init.adminUser.password="${SUPERSET_ADMIN_USER_PASS}" \
     --values superset-values.yaml \
     -n superset
 
