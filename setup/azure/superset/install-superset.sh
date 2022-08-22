6e721ccc541ae846fc2f520f73c6cc1a795e3729
Chiranga Alwis <chirangaalwis@gmail.com>
2022-08-22 23:31:20 +0530

    [Superset] Add missing license headers

--- setup/azure/superset/install-superset.sh
+++ setup/azure/superset/install-superset.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 Inc. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+# Deploy Apache Superset Kubernetes workload
 
 SUPERSET_DB_PASS=$(az keyvault secret show --name postgresql-SUPERSET-DB-PASSWORD --vault-name "${CSI-KEYVAULT-NAME}" --query value -o tsv)
 SUPERSET_ADMIN_USER_PASS=$(az keyvault secret show --name superset-ADMIN-PASSWORD --vault-name "${CSI-KEYVAULT-NAME}" --query value -o tsv)
