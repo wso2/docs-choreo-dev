83454c45a1a5b12aebbb8341d11baf20361855ab
Chiranga Alwis <chirangaalwis@gmail.com>
2022-08-22 23:23:52 +0530

    [Superset] Add custom config Secret

--- setup/azure/superset/install-superset.sh
+++ setup/azure/superset/install-superset.sh
@@ -11,12 +11,14 @@ helm registry login choreocontrolplane.azurecr.io --username "${HELM_ACR_USERNAM
 # Pull Helm Chart
 helm pull oci://choreocontrolplane.azurecr.io/helm/superset --version 0.7.1
 
+# Deploy customized configuration files + dependent service secrets via a Kubernetes Secret
+kubectl apply -f superset-custom-config-secret.yaml
+
 # Install
 helm upgrade --install superset superset-0.7.1.tgz \
-    --set supersetNode.connections.db_pass="${SUPERSET_DB_PASS}" \
     --set init.adminUser.password="${SUPERSET_ADMIN_USER_PASS}" \
     --values superset-values.yaml \
     -n superset
 
-# Configure netpols
+# Deploy network policies for Superset deployment
 kubectl apply -f superset-netpols.yaml
