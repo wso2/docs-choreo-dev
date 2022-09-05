53da5c8545ef42adf61c82163c11c655c8542e51
uvindra <uvindra_dj@yahoo.com>
2022-09-05 17:02:49 +0530

    Fix changes in component creation UI

--- e2e-tests-cypress/cypress/support/console/pages/templates/rest-api-temp.ts
+++ e2e-tests-cypress/cypress/support/console/pages/templates/rest-api-temp.ts
@@ -11,25 +11,27 @@
  * associated services.
  */
 
-
-
 export class RestAPITemplate {
   static selectHttpAPITemplate() {
     cy.get('[data-testid="project-template-list-httpApi"]').click();
   }
 
-  static createApiFromScratch(componentName: string, description: string, isEPLogin: boolean = false, accessModes: string = "external") {
+  static createApiFromScratch(
+    componentName: string,
+    description: string,
+    isEPLogin: boolean = false,
+    accessModes: string = "external"
+  ) {
     cy.get('[role="dialog"] ul>div:nth-child(1)').click();
     cy.get('[name="name"]').clear().type(componentName);
     cy.get('input[name="description"]').clear().type(description);
-    cy.get('.MuiInputBase-formControl>div:not([id])').click()
-    cy.get(`li[data-value=${accessModes}]`).click()
-    cy.get('[data-cyid="create-api-from-scratch-next"]').click();
+    cy.get(".MuiInputBase-formControl>div:not([id])").click();
+    cy.get(`li[data-value=${accessModes}]`).click();
     cy.get('[data-cyid="choreo-managed-repo-radio-btn"]').click();
     cy.get('[data-testid="create-api-from-scratch-submit"]').click();
 
     if (isEPLogin) {
-      cy.setCookie("fidpId", "EnterpriseIDP")
+      cy.setCookie("fidpId", "EnterpriseIDP");
     } else {
       cy.setCookie("fidpId", "choreoe2etest");
     }
