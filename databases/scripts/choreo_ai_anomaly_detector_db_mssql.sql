8c5d52a40de57b5d3a3854b659878cb3e7cc3280
isuruRuhu <isuru.ruhu@gmail.com>
2022-09-14 18:44:26 +0530

    Change to all capitals

--- databases/scripts/choreo_ai_anomaly_detector_db_mssql.sql
+++ databases/scripts/choreo_ai_anomaly_detector_db_mssql.sql
@@ -7,36 +7,36 @@ END;
 GO
 
 -- Create anomaly detector code_version table
-IF NOT EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[code_version]') AND TYPE IN (N'U'))
-CREATE TABLE code_version (
-             code_version_id INTEGER IDENTITY(1,1) NOT NULL,
-             org_id VARCHAR (255)  NOT NULL,
-             obs_id VARCHAR (255)  NOT NULL,
-             obs_version VARCHAR (255) NOT NULL,
-             release_id VARCHAR (255) NOT NULL,
-             PRIMARY KEY (code_version_id)
+IF NOT EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[CODE_VERSION]') AND TYPE IN (N'U'))
+CREATE TABLE CODE_VERSION (
+             CODE_VERSION_ID INTEGER IDENTITY(1,1) NOT NULL,
+             ORG_ID VARCHAR (255)  NOT NULL,
+             OBS_ID VARCHAR (255)  NOT NULL,
+             OBS_VERSION VARCHAR (255) NOT NULL,
+             RELEASE_ID VARCHAR (255) NOT NULL,
+             PRIMARY KEY (CODE_VERSION_ID)
 );
 
 -- Create anomaly detector alert table
-IF NOT EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[resource]') AND TYPE IN (N'U'))
-CREATE TABLE alert (
-             resource_id INTEGER IDENTITY(1,1) NOT NULL,
-             code_version_id INTEGER,
-             service_name VARCHAR (255) NOT NULL,
-             function_name VARCHAR (255) NOT NULL,
-             resource_accessor VARCHAR (255) NOT NULL,
-             PRIMARY KEY (resource_id)
+IF NOT EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[RESOURCE]') AND TYPE IN (N'U'))
+CREATE TABLE RESOURCE (
+             RESOURCE_ID INTEGER IDENTITY(1,1) NOT NULL,
+             CODE_VERSION_ID INTEGER,
+             SERVICE_NAME VARCHAR (255) NOT NULL,
+             FUNCTION_NAME VARCHAR (255) NOT NULL,
+             RESOURCE_ACCESSOR VARCHAR (255) NOT NULL,
+             PRIMARY KEY (RESOURCE_ID)
 );
 
 -- Create anomaly detector alert table
-IF NOT EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[alert]') AND TYPE IN (N'U'))
-CREATE TABLE alert (
-             alert_id INTEGER IDENTITY(1,1) NOT NULL,
-             resource_id INTEGER,
-             anomaly_timestamp DATETIME NOT NULL,
-             alert_type VARCHAR (255) NOT NULL,
-             properties JSON NOT NULL,
-             PRIMARY KEY (code_version_id)
+IF NOT EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[DBO].[ALERT]') AND TYPE IN (N'U'))
+CREATE TABLE ALERT (
+             ALERT_ID INTEGER IDENTITY(1,1) NOT NULL,
+             RESOURCE_ID INTEGER,
+             ANOMALY_TIMESTAMP DATETIME NOT NULL,
+             ALERT_TYPE VARCHAR (255) NOT NULL,
+             PROPERTIES VARCHAR (2000) NOT NULL,
+             PRIMARY KEY (ALERT_ID)
 );
 
 
