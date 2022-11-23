3a255f9a79465014aaef914ef8fd50401ceff674
uvindra <uvindra_dj@yahoo.com>
2022-11-23 14:28:00 +0530

    Fix for #16574
    Fix for https://github.com/wso2-enterprise/choreo/issues/16574

--- e2e-tests-cypress/cypress/e2e-smoke/devportal/1.2-devportal-custom-domain-test.ts
+++ e2e-tests-cypress/cypress/e2e-smoke/devportal/1.2-devportal-custom-domain-test.ts
@@ -34,141 +34,105 @@ import { ComponentListingPage } from "../../support/console/pages/component/comp
 const CUSTOM_DOMAIN = Cypress.env("devportalCustomDomain");
 
 describe("Create and deploy a component to test developer portal with custom domain", () => {
+  before(() => {
+    ConsoleLoginPage.login();
+  });
 
-    before(() => {
-        ConsoleLoginPage.login();
-    });
+  it("Create and deploy a component", () => {
+    const API_Name = Utils.generateComponentName("oas");
+    cy.task("setAPIName", API_Name);
+    DevPortalHelper.createDeployHttpProxyComponent(API_Name);
+  });
 
-    it("Create and deploy a component", () => {
-        const API_Name = Utils.generateComponentName("oas");
-        cy.task('setAPIName', API_Name);
-        DevPortalHelper.createDeployHttpProxyComponent(API_Name);
-    });
+  it("Add a developer portal custom domain", () => {
+    ChoreoHomePage.navigateToSettings();
+    DomainsComponents.navigateToDomainsSettings();
+    DomainsComponents.navigateToDevPortalCustomDomain();
+    DomainsComponents.deleteDevportalDomainIfExists(CUSTOM_DOMAIN);
+    DomainsComponents.createDevportalDomain(CUSTOM_DOMAIN);
+  });
 
-    after(() => {
-        ChoreoHomePage.logout();
-    });
-});
-
-describe("Add developer portal custom domain", () => {
-
-    before(() => {
-        ConsoleLoginPage.login();
-        ChoreoHomePage.navigateToSettings();
-        DomainsComponents.navigateToDomainsSettings();
-        DomainsComponents.navigateToDevPortalCustomDomain();
-        DomainsComponents.deleteDevportalDomainIfExists(CUSTOM_DOMAIN);
-    });
-
-    after(() => {
-        ChoreoHomePage.logout();
-    });
-
-    it("Add a developer portal custom domain", () => {
-        DomainsComponents.createDevportalDomain(CUSTOM_DOMAIN);
-    });
-});
-
-describe("Visit developer portal with custom domain", () => {
-    it("Access developer portal with custom domain", () => {
-        DomainsComponents.accessDevportalWithCustomDomain(CUSTOM_DOMAIN);
-    });
+  after(() => {
+    ChoreoHomePage.logout();
+  });
 });
 
 describe("Login and test developer portal with custom domain", () => {
-    const OPERATION_USERS = "intensity";
-    const appName = generateAppName("-e2etest");
+  const OPERATION_USERS = "intensity";
+  const appName = generateAppName("-e2etest");
 
-    before(() => {
-        DevportalLoginPage.loginToDevportal(CUSTOM_DOMAIN);
+  before(() => {
+    DevportalLoginPage.loginToDevportal(CUSTOM_DOMAIN);
+  });
+
+  after(() => {
+    DevPortalHomePage.logout();
+  });
+
+  it("Test in devportal", () => {
+    cy.task("getAPIName").then((an) => {
+      let API_Name = an as string;
+      DevPortalHomePage.navigateToApisPage();
+      Apis.searchApiAndSelect(API_Name);
+      DevPortalHomePage.navigateToPerApiView(API_Name);
+      Apis.verifyAPIname().should("eq", API_Name);
     });
+  });
 
-    after(() => {
-        DevPortalHomePage.logout();
+  it("Add and delete comment for the API", () => {
+    ApiOverview.addCommentToApi("Test comment from Cypress Test Runner");
+    ApiOverview.deleteComment();
+  });
+
+  it("Add and modify ratings of the API", () => {
+    ApiOverview.openRatings();
+    ApiOverview.addRatings();
+    ApiOverview.validateRating();
+  });
+
+  it("Generate credentials and tryout the API", () => {
+    ApiCredentials.navigateCredentialsTab();
+    ApiCredentials.generateCredentials();
+    TryOut.navigateToTryOutMenu();
+    TryOut.GenerateAccessToken();
+    TryOut.SelectResource("GET", OPERATION_USERS);
+    TryOut.TryoutAPI();
+    TryOut.ExecuteResourceFunction();
+    TryOut.GetResponse();
+  });
+
+  it("Verify the downloaded SDK file", () => {
+    cy.task("getAPIName").then((API_Name) => {
+      const sdkFile = API_Name + "_1.0.0_android.zip";
+      APISdk.downloadSDK(sdkFile);
     });
+  });
 
-    it("Test in devportal", () => {
-        cy.task('getAPIName').then(an => {
-            let API_Name = an as string
-            DevPortalHomePage.navigateToApisPage();
-            Apis.searchApiAndSelect(API_Name);
-            DevPortalHomePage.navigateToPerApiView(API_Name);
-            Apis.verifyAPIname().should("eq", API_Name);
-        })
-
+  it("Create a consumer application and tryout an API", () => {
+    cy.task("getAPIName").then((an) => {
+      let API_Name = an as string;
+      DevPortalHomePage.navigateToAppsPage();
+      AppsList.createAnApplication(appName);
+      ProductionKeys.generateTestToken();
+      Subscriptions.addSubscriptionToApplication(API_Name);
+      Subscriptions.validateResubscribingApi(API_Name);
     });
+  });
 
-    it("Add and delete comment for the API", () => {
-        ApiOverview.addCommentToApi("Test comment from Cypress Test Runner");
-        ApiOverview.deleteComment();
-    });
-
-    it("Add and modify ratings of the API", () => {
-        ApiOverview.openRatings();
-        ApiOverview.addRatings();
-        ApiOverview.validateRating();
-    });
-
-    it("Generate credentials and tryout the API", () => {
-        ApiCredentials.navigateCredentialsTab();
-        ApiCredentials.generateCredentials();
-        TryOut.navigateToTryOutMenu();
-        TryOut.GenerateAccessToken();
-        TryOut.SelectResource("GET", OPERATION_USERS);
-        TryOut.TryoutAPI();
-        TryOut.ExecuteResourceFunction();
-        TryOut.GetResponse();
-    });
-
-    it("Verify the downloaded SDK file", () => {
-        cy.task('getAPIName').then(API_Name => {
-            const sdkFile = API_Name + "_1.0.0_android.zip";
-            APISdk.downloadSDK(sdkFile);
-        })
-
-    });
-
-    it("Create a consumer application and tryout an API", () => {
-        cy.task('getAPIName').then(an => {
-            let API_Name = an as string
-            DevPortalHomePage.navigateToAppsPage();
-            AppsList.createAnApplication(appName);
-            ProductionKeys.generateTestToken();
-            Subscriptions.addSubscriptionToApplication(API_Name);
-            Subscriptions.validateResubscribingApi(API_Name);
-        })
-
-    });
-
-    it("Delete a consumer application", () => {
-        TryOut.DeleteApplication(appName);
-    });
+  it("Delete a consumer application", () => {
+    TryOut.DeleteApplication(appName);
+  });
 });
 
 describe("Delete added custom domain", () => {
-    before(() => {
-        ConsoleLoginPage.login();
-        ChoreoHomePage.navigateToSettings();
-        DomainsComponents.navigateToDomainsSettings();
-        DomainsComponents.navigateToDevPortalCustomDomain();
-    });
+  before(() => {
+    ConsoleLoginPage.login();
+    ChoreoHomePage.navigateToSettings();
+    DomainsComponents.navigateToDomainsSettings();
+    DomainsComponents.navigateToDevPortalCustomDomain();
+  });
 
-    after(() => {
-
-        cy.task('getAPIName').then(an => {
-            let API_Name = an as string
-            ChoreoHomePage.logout();
-            ChoreoHomePage.navigateToComponents()
-            ComponentListingPage.deleteComponent(API_Name)
-        })
-    });
-
-    it("Delete added custom domain", () => {
-        DomainsComponents.deleteCreatedCustomDomain(CUSTOM_DOMAIN);
-    });
+  it("Delete added custom domain", () => {
+    DomainsComponents.deleteCreatedCustomDomain(CUSTOM_DOMAIN);
+  });
 });
-
-
-
-
-
--- e2e-tests-cypress/cypress/support/console/pages/component/common/domains-components.ts
+++ e2e-tests-cypress/cypress/support/console/pages/component/common/domains-components.ts
@@ -14,67 +14,73 @@
 import { Utils } from "../../../utils";
 
 export class DomainsComponents {
-    static keyName = Utils.generateKeyName("key");
+  static keyName = Utils.generateKeyName("key");
 
-    static navigateToDomainsSettings() {
-        cy.get('[data-cy="/domains"]').click();
-    }
+  static navigateToDomainsSettings() {
+    cy.get('[data-cy="/domains"]').click();
+  }
 
-    static navigateToDevPortalCustomDomain() {
-        cy.get('[data-cy="/domains/devportal"]').click();
-    }
+  static navigateToDevPortalCustomDomain() {
+    cy.get('[data-cy="/domains/devportal"]').click();
+  }
 
-    static deleteDevportalDomainIfExists(domainName: string) {
-        cy.get('[data-cyid="devportal-custom-domain-search-field"]').trigger("mouseover");
-        cy.get('[data-cyid="search-app"]').clear().type(domainName);
-        cy.wait(2000);
-        cy.get("td").then(($domain) => {
-            if (!$domain.text().includes("No records to display")) {
-                cy.contains("td", "Managed by Choreo").should("be.visible");
-                this.deleteSelectedDomain(domainName);
-            }
-        });
-    }
-
-    private static deleteSelectedDomain(domainName: string) {
-        cy.get('[class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignLeft"]').dblclick();
-        cy.contains('Delete').click({ force: true });
-        cy.log("Deleting the created Domain");
-        cy.get('[data-cyid="btn-confirmation-dialog-blue"]').click();
-        cy.contains("td", domainName).should("not.exist");
-        cy.log("Domain deleted successfully");
-    }
-
-    static deleteCreatedCustomDomain(domainName: string) {
-        cy.get('[data-cyid="devportal-custom-domain-search-field"]').trigger("mouseover");
-        cy.get('[data-cyid="search-app"]').clear().type(domainName);
-        cy.contains("td", domainName).should("be.visible");
+  static deleteDevportalDomainIfExists(domainName: string) {
+    cy.get('[data-cyid="devportal-custom-domain-search-field"]').trigger(
+      "mouseover"
+    );
+    cy.get('[data-cyid="search-app"]').clear().type(domainName);
+    cy.wait(2000);
+    cy.get("td").then(($domain) => {
+      if (!$domain.text().includes("No records to display")) {
+        cy.contains("td", "Managed by Choreo").should("be.visible");
         this.deleteSelectedDomain(domainName);
-    }
+      }
+    });
+  }
 
-    static createDevportalDomain(domainName: string) {
-        cy.get('[data-cyid="btn-add-domain"]').click();
-        cy.contains("Create Custom Domain").should("be.visible");
-        cy.log("Creating a dev portal custom domain");
-        cy.get('[data-cyid="text-field-domain-name"]').type(domainName);
-        cy.get('[data-cyid="btn-verify-domain"]').click({ force: true });
-        cy.get('[data-cyid="btn-verify-domain"]').should("not.exist");
-        cy.get('[data-cyid="btn-next"]').should("exist");
-        cy.log("Verified the CNAME mapping availability");
-        cy.get('[data-cyid="btn-next"]').click({ force: true });
-        cy.contains("Select TLS Certificate Provider").should("be.visible");
-        cy.get('[data-cyid="lets-encrypt-certificate-tile"]').click();
-        cy.get('[data-cyid="btn-add"]').click({ force: true });
-        cy.get('[data-cyid="btn-add"]').should("not.exist");
-        cy.contains("td", domainName).should("be.visible");
-        cy.log("Created the custom domain successfully");
-    }
+  private static deleteSelectedDomain(domainName: string) {
+    cy.get(
+      '[class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignLeft"]'
+    ).dblclick();
+    cy.contains("Delete").click({ force: true });
+    cy.log("Deleting the created Domain");
+    cy.get('[data-cyid="btn-confirmation-dialog-blue"]').click();
+    cy.get('[data-cyid="search-app"]').should("be.visible");
+    cy.contains("td", domainName).should("not.exist");
+    cy.log("Domain deleted successfully");
+  }
 
-    static accessDevportalWithCustomDomain(domainName: string) {
-        cy.visit(domainName);
-        // cy.url().should('include', domainName);
-        // cy.get('[data-cyid="sign-in-with-github"]').should("be.visible");
-        // cy.get('[data-cyid="sign-in-with-google"]').should("be.visible");
-        // cy.get('[data-cyid="sign-in-with-microsoft"]').should("be.visible");
-    }
+  static deleteCreatedCustomDomain(domainName: string) {
+    cy.get('[data-cyid="devportal-custom-domain-search-field"]').trigger(
+      "mouseover"
+    );
+    cy.get('[data-cyid="search-app"]').clear().type(domainName);
+    cy.contains("td", domainName).should("be.visible");
+    this.deleteSelectedDomain(domainName);
+  }
+
+  static createDevportalDomain(domainName: string) {
+    cy.get('[data-cyid="btn-add-domain"]').click();
+    cy.contains("Create Custom Domain").should("be.visible");
+    cy.log("Creating a dev portal custom domain");
+    cy.get('[data-cyid="text-field-domain-name"]').type(domainName);
+    cy.get('[data-cyid="btn-verify-domain"]').click({ force: true });
+    cy.get('[data-cyid="btn-verify-domain"]').should("not.exist");
+    cy.get('[data-cyid="btn-next"]').should("exist");
+    cy.log("Verified the CNAME mapping availability");
+    cy.get('[data-cyid="btn-next"]').click({ force: true });
+    cy.contains("Select TLS Certificate Provider").should("be.visible");
+    cy.get('[data-cyid="lets-encrypt-certificate-tile"]').click();
+    cy.get('[data-cyid="btn-add"]').should("be.visible").click({ force: true });
+    cy.contains("td", domainName).should("be.visible");
+    cy.log("Created the custom domain successfully");
+  }
+
+  static accessDevportalWithCustomDomain(domainName: string) {
+    cy.visit(domainName);
+    // cy.url().should('include', domainName);
+    // cy.get('[data-cyid="sign-in-with-github"]').should("be.visible");
+    // cy.get('[data-cyid="sign-in-with-google"]').should("be.visible");
+    // cy.get('[data-cyid="sign-in-with-microsoft"]').should("be.visible");
+  }
 }
