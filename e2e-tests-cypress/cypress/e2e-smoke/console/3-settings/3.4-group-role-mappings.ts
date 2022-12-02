e4877c61dfec1abbd571eaa9cf1a86d51c979a98
anukeshi <anukeshi96@gmail.com>
2022-12-02 09:40:08 +0530

    Fix license header

--- e2e-tests-cypress/cypress/e2e-smoke/console/3-settings/3.4-group-role-mappings.ts
+++ e2e-tests-cypress/cypress/e2e-smoke/console/3-settings/3.4-group-role-mappings.ts
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
  *
- * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
  * Dissemination of any information or reproduction of any material contained
  * herein is strictly forbidden, unless permitted by WSO2 in accordance with
  * the WSO2 Commercial License available at http://wso2.com/licenses.
