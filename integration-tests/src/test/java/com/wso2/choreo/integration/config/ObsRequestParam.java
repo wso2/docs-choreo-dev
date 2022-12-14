6f0a67e2cba6a75805af3344e78a296ab82643cc
dasun <dasun@wso2.com>
2022-12-14 09:21:06 +0530

    ui chagnes

---
+++ integration-tests/src/test/java/com/wso2/choreo/integration/config/ObsRequestParam.java
@@ -0,0 +1,17 @@
+package com.wso2.choreo.integration.config;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ObsRequestParam {
+
+    private String startTime;
+    private String endTime;
+    private String releaseId;
+    private String namespace;
+    private String sort;
+    private String limit;
+    private String bin;
+}
