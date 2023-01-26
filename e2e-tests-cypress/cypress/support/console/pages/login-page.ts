a96b53ce65ad6a6fb68cf6c67e46f7691780e472
isuruPiyarathna <isurupi@wso2.com>
2023-01-26 11:05:49 +0530

    fix 6.0 test

--- e2e-tests-cypress/cypress/support/console/pages/login-page.ts
+++ e2e-tests-cypress/cypress/support/console/pages/login-page.ts
@@ -72,7 +72,6 @@ export class LoginPage {
     });
   }
   static reLoginToChoreo() {
-    //const componentURL = Cypress.env(`componentURL`);
     const componentURL = `${Cypress.env('baseUrl')}/organizations/${Cypress.env('choreoOrgHandle')}/home?profile=default`
     const common =
       Cypress.env(`commonAuthId`) != null
