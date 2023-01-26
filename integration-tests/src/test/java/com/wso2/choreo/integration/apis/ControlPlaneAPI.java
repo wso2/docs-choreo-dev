1420358ccebe169d4971e716365216404170564d
uvindra <uvindra_dj@yahoo.com>
2023-01-26 18:19:46 +0530

    Clean old connectors & enable parallel runs

--- integration-tests/src/test/java/com/wso2/choreo/integration/apis/ControlPlaneAPI.java
+++ integration-tests/src/test/java/com/wso2/choreo/integration/apis/ControlPlaneAPI.java
@@ -15,6 +15,8 @@ public abstract class ControlPlaneAPI {
 
     protected static final String STS_ENDPOINT = Configuration.getConfig(ConfigDefinition.STS_ENDPOINT);
     protected static final String CHOREO_EP = Configuration.getConfig(ConfigDefinition.CHOREO_ENDPOINT);
+
+    protected static final String BAL_REGISTRY_URL = Configuration.getConfig(ConfigDefinition.BAL_REGISTRY_URL);
     protected static final String GH_URL = Configuration.getConfig(ConfigDefinition.GITHUB_ENDPOINT);
     protected static final String CHOREO_CP_GW_ENDPOINT = Configuration.getConfig(ConfigDefinition.CHOREO_CP_GW_ENDPOINT);
     protected static final String ORG_UUID = Configuration.getConfig(ConfigDefinition.TEST_CHOREO_ORG_UUID);
