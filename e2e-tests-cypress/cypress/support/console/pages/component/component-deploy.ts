5743d82cd08cfbc21f69ded7710f4e2df7604c65
uvindra <uvindra_dj@yahoo.com>
2023-01-27 10:00:43 +0530

    Fix Deployment card rerendering issue

--- e2e-tests-cypress/cypress/support/console/pages/component/component-deploy.ts
+++ e2e-tests-cypress/cypress/support/console/pages/component/component-deploy.ts
@@ -28,6 +28,11 @@ export class ComponentDeployPage {
     cy.get('[data-testid="btn-stop"]', { timeout: 600000 }).should(
       "be.visible"
     );
+    // UI re-rendering takes place, so recheck if the Stop button has been loaded after a short wait
+    // to ensure rendering completes before checking the deployment status
+    cy.wait(600);
+    cy.get('[data-testid="btn-stop"]').should("be.visible");
+
     cy.get('[data-cyid="deployment-status"]', { timeout: 360000 }).contains(
       "Active",
       { timeout: 360000 }
@@ -122,6 +127,11 @@ export class ComponentDeployPage {
     cy.get('[data-testid="btn-stop"]', { timeout: 360000 }).should(
       "be.visible"
     );
+    // UI re-rendering takes place, so recheck if the Stop button has been loaded after a short wait
+    // to ensure rendering completes before checking the deployment status
+    cy.wait(600);
+    cy.get('[data-testid="btn-stop"]').should("be.visible");
+
     cy.get('[data-cyid="deployment-status"]', { timeout: 360000 }).contains(
       "Active",
       { timeout: 360000 }
