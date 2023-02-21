6a87ea7fd873e35ade233975e300e3ff92f0451e
Avishka <62095876+Avishka217@users.noreply.github.com>
2023-02-21 12:35:49 +0530

    Remove Component Deployment from spec

--- e2e-tests-cypress/cypress/e2e-smoke/console/7-apimperpective/7.0-create-rest-api-apim-perspective.ts
+++ e2e-tests-cypress/cypress/e2e-smoke/console/7-apimperpective/7.0-create-rest-api-apim-perspective.ts
@@ -65,10 +65,5 @@ describe("Verify project creation functionality", () => {
     GraphQL.createComponent(PROJECT_NAME, REPO_NAME, componentData, GraphQLQueryBuilder.getRestComponentCreationQuery)
   });
 
-  it("Verify component deployment", () => {
-    ComponentListingPage.visitToAComponent(COMPONENT_NAME);
-    ComponentOverviewPage.navigateToDeploy();
-    ComponentDeployPage.deployToDev();
-  });
 
 });
