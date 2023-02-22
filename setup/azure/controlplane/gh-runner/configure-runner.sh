ab78a3ebae09a6f45b2dd8e925e2120d7d3e9915
udhanMti <udhan@wso2.com>
2023-02-22 23:18:52 +0530

    Update image repository in 3rd party components

--- setup/azure/controlplane/gh-runner/configure-runner.sh
+++ setup/azure/controlplane/gh-runner/configure-runner.sh
@@ -27,6 +27,6 @@ helm upgrade --install actions-runner-controller actions-runner-controller-0.20.
     --set=logLevel="${LOG_LEVEL}" \
     --set=githubWebhookServer.logLevel="${LOG_LEVEL}" \
     --set=image.repository=choreocontrolplane.azurecr.io/summerwind/actions-runner-controller \
-    --set=image.actionsRunnerRepositoryAndTag=choreocontrolplane.azurecr.io/summerwind/actions-runner-controller \
-    --set=image.dindSidecarRepositoryAndTag=choreocontrolplane.azurecr.io/dind:latest \
+    --set=image.actionsRunnerRepositoryAndTag=choreocontrolplane.azurecr.io/summerwind/actions-runner:latest \
+    --set=image.dindSidecarRepositoryAndTag=choreocontrolplane.azurecr.io/dind:latest \  ## dind:latest for stg & prod || docker:dind for dev
     --set=metrics.proxy.image.repository=choreocontrolplane.azurecr.io/brancz/kube-rbac-proxy
