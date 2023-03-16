ce8ad294204d9e1dc52ee5f65afcbe241e51d338
Janaka Sandaruwan <janakasandaruwan1996@gmail.com>
2023-03-16 15:03:37 +0530

    Add database scripts

--- databases/scripts/choreo_app_db_mssql.sql
+++ databases/scripts/choreo_app_db_mssql.sql
@@ -1624,7 +1624,7 @@ CREATE TABLE [dbo].[permission]
     [id] [int] IDENTITY(1,1) NOT NULL ,
     [handle][varchar](255) NOT NULL,
     [display_name][varchar](255) NOT NULL,
-    [domain_area][varchar](50) NOT NULL CHECK (domain_area IN('APIM-ADMIN','APIM-PUBLISHER','APIM-SUBSCRIBER','BC','AI','BILLING','ACCOUNT-MANAGE')),
+    [domain_area][varchar](50) NOT NULL CHECK (domain_area IN('APIM-ADMIN','APIM-PUBLISHER','APIM-SUBSCRIBER','BC','AI','BILLING','ACCOUNT-MANAGE', 'CONFIGURATIONS-MANAGEMENT')),
     [description] [varchar](255) NULL,
     [created_at] [datetime]   NOT NULL DEFAULT CURRENT_TIMESTAMP,
     [updated_at] [datetime]   NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -1967,3 +1967,5 @@ INSERT INTO permission (display_name,handle,domain_area,description) VALUES ('Ma
 INSERT INTO permission (display_name,handle,domain_area,description) VALUES ('Manage Users','choreo:user_manage','ACCOUNT-MANAGE','Add and remove users');
 INSERT INTO permission (display_name,handle,domain_area,description) VALUES ('Manage Roles','choreo:role_manage','ACCOUNT-MANAGE','Create, update and delete roles');
 INSERT INTO permission (display_name,handle,domain_area,description) VALUES ('Manage Organization','choreo:organization_manage','ACCOUNT-MANAGE','Create, update and delete organization');
+INSERT INTO permission (display_name,handle,domain_area,description) VALUES ('Manage Global Configs','urn:choreocontrolplane:globalconfigurationservic:global_config_manage','CONFIGURATIONS-MANAGEMENT','Create, Edit and Delete Global Configs'),
+('View Global Configs','urn:choreocontrolplane:globalconfigurationservic:global_config_view','CONFIGURATIONS-MANAGEMENT','View Global Configs'),('Create Global Configs','urn:choreocontrolplane:globalconfigurationservic:global_config_create','CONFIGURATIONS-MANAGEMENT','Create Global Configs');
