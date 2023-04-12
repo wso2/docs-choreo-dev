84abd4d8083a498e38cfbad6899f21d871ac34b4
Viraj Salaka <viraj.salaka@gmail.com>
2023-04-12 09:56:55 +0530

    sync with main branch

--- e2e-tests-cypress/cypress/e2e-smoke/devportal/1.2-devportal-custom-domain-test.ts
+++ e2e-tests-cypress/cypress/e2e-smoke/devportal/1.2-devportal-custom-domain-test.ts
@@ -74,7 +74,7 @@ describe("Create and deploy a component to test developer portal with custom dom
   });
 });
 
-describe.skip("Login and test developer portal with custom domain", () => {
+describe("Login and test developer portal with custom domain", () => {
   const OPERATION_USERS = "intensity";
   const appName = generateAppName("-e2etest");
 
--- e2e-tests-cypress/cypress/support/console/pages/projects/projects-listing-page.ts
+++ e2e-tests-cypress/cypress/support/console/pages/projects/projects-listing-page.ts
@@ -52,13 +52,8 @@ export class ProjectListingPage {
 
     cy.get('body').then(bdy => {
       if (bdy.find('[data-cyid="create-project-card"]').length > 0) {
-<<<<<<< HEAD
-        cy.get('[data-cyid="search-icon"]').eq(1).click()
-        cy.get('[placeholder="Search"]').type(`${projectName}{enter}`)
-=======
         cy.get('[data-cyid="search-icon"]').eq(0).click()
         cy.get('[data-testid="search-field"]').type(`${projectName}{enter}`)
->>>>>>> main
         cy.contains(projectName).click()
       } else {
 
