18494074a9b1c8218c8a595c88ce1181c83b8d5b
isuruPiyarathna <isurupi@wso2.com>
2023-04-27 16:21:28 +0530

    add test id

--- e2e-tests-cypress/cypress/support/console/pages/component/component-manage-page.ts
+++ e2e-tests-cypress/cypress/support/console/pages/component/component-manage-page.ts
@@ -226,8 +226,7 @@ export class ComponentAPILifecycle {
   static applyConfiguration() {
     cy.get('[data-cyid="btn-save-settings"]').click();
     cy.get("button").contains("Apply").click().wait(2000);
-    cy.get('[data-cyid="btn-delete-settings"]').should("be.visible");
-    cy.get('.MuiBadge-root > .MuiButtonBase-root > .MuiButton-label').should("be.visible");
+    cy.get('[data-testid="operation"]').should("be.visible");
     cy.get('[data-cyid="btn-delete-settings"]').should("be.visible");
     cy.wait(4000);
   }
