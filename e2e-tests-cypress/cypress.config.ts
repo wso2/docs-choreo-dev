dc23e674add38cd6ad62a08b9cc52cbf35a04570
isuruPiyarathna <isurupi@wso2.com>
2023-05-04 11:49:54 +0530

    Downgrade cypress version

--- e2e-tests-cypress/cypress.config.ts
+++ e2e-tests-cypress/cypress.config.ts
@@ -46,7 +46,6 @@ export default defineConfig({
       return config;
     },
     specPattern: "cypress/e2e-smoke//./**/*.ts",
-    testIsolation: false
   },
   env: {
     FAIL_FAST_STRATEGY: "spec",
