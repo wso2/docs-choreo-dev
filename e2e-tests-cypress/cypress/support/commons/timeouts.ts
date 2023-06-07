2cee6d6dd7dd57c9f0160fe15e78c48e0a302101
uvindra <uvindra_dj@yahoo.com>
2023-06-07 13:51:49 +0530

    Unified menu insights & other fixes

--- e2e-tests-cypress/cypress/support/commons/timeouts.ts
+++ e2e-tests-cypress/cypress/support/commons/timeouts.ts
@@ -11,10 +11,10 @@
  * associated services.
  */
 
-
-
 export const VERY_SHORT_TIME = { timeout: 10000 }; // 10 seconds
-export const SHORT_TIME = { timeout: 60000 }      // 1 minute
+export const SHORT_TIME = { timeout: 60000 }; // 1 minute
 export const MEDIUM_TIME = { timeout: 180000 }; // 3 minutes
 export const LONG_TIME = { timeout: 360000 }; // 6 minutes
 export const VERY_LONG_TIME = { timeout: 600000 }; // 10 minutes
+
+export const MENU_RENDERING_TIME = 300; // 0.3 seconds
