2d6db693a47a76f62435b2f7ca1f152ead51784d
SanduDS <sandaruwandanushka@gmail.com>
2023-06-14 16:11:46 +0530

    Fix e2e test - 1.3 dev portal public api test failures

--- e2e-tests-cypress/cypress/support/devportal/pages/login/login-page.ts
+++ e2e-tests-cypress/cypress/support/devportal/pages/login/login-page.ts
@@ -34,9 +34,6 @@ export class LoginPage {
   }
 
   static visitToDevportalOrgPublicApis(): void {
-    cy.clearAllCookies()
-    cy.clearLocalStorage()
-    cy.clearAllSessionStorage()
     const loginURL = Cypress.env("devportalLoginURL") + "/" + handle;
     cy.visit(loginURL);
     cy.get('[data-testid="home-appbar-btn"]').should("exist");
