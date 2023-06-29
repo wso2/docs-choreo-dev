f840705746b598704631a95d73671cf88e12cc7f
Oshada Jayasinghe <oshadajayasinghe@gmail.com>
2023-06-29 15:43:48 +0530

    Shorten component names

--- integration-tests/src/test/java/com/wso2/choreo/integration/tests/integrationComponent/TestCreateIntegrationRestComponentFromRoot.java
+++ integration-tests/src/test/java/com/wso2/choreo/integration/tests/integrationComponent/TestCreateIntegrationRestComponentFromRoot.java
@@ -93,7 +93,7 @@ public class TestCreateIntegrationRestComponentFromRoot extends TestNGCitrusSpri
     public void createComponent_TestCreateIntegrationRestComponentFromRoot() throws Exception {
 
         // Creating component
-        String componentName = "integrationrestcomponent";
+        String componentName = "atc".concat(String.valueOf(new Date().getTime()));
         final String repoName = "synaps-api-project-sample";
         final String repoBranch = "main";
         String srcGitHubURL = Constant.GITHUB_URL.concat(githubOrg).concat("/").concat(repoName);
