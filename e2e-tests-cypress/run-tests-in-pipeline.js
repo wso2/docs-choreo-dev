21e8e7366ca91a917b27d68f76eb546537fe221a
uvindra <uvindra_dj@yahoo.com>
2023-09-18 17:20:41 +0530

    Support auto promotion on test pass count

---
+++ e2e-tests-cypress/run-tests-in-pipeline.js
@@ -0,0 +1,37 @@
+const cypress = require('cypress');
+
+const args = process.argv.slice(2);
+
+if (args.length == 2) {
+  const buildNumber = args[0];
+  const buildId = args[1];
+
+  runTests(buildNumber, buildId);
+} else {
+  printUsage();
+}
+
+async function runTests(buildNumber, buildId) {
+  cypress
+    .run({
+      headless: true,
+      record: true,
+      parallel: true,
+      tag: buildNumber,
+      ciBuildId: buildId,
+      spec: ['cypress/e2e-stable/devportal/*.ts','cypress/e2e-stable/console/**/*','cypress/e2e-unstable/devportal/*.ts','cypress/e2e-unstable/console/**/*'],
+    })
+    .then((results) => {
+      console.log(results.totalPassed);
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
+
+function printUsage() {
+    console.log("\n");
+    console.log("usage: node run-tests-in-pipeline.js <buildNumber> <buildId>")
+    process.exit(1);
+}
