8061928ca512cc903ac4d51ba151b977480ad3c5
isuruPiyarathna <isurupi@wso2.com>
2023-09-20 12:26:28 +0530

    Code refactoring

--- e2e-tests-cypress/cypress/e2e-stable/console/1-component/1.1-create-api-from-oas-flow.ts
+++ e2e-tests-cypress/cypress/e2e-stable/console/1-component/1.1-create-api-from-oas-flow.ts
@@ -46,7 +46,7 @@ describe("Choreo APIM publisher scenarios", () => {
   const OPERATION = "intensity";
 
   before(() => {
-    LoginPage.login(true);
+    LoginPage.login();
   });
   after(() => {
     ChoreoHomePage.logout();
@@ -131,7 +131,12 @@ describe("Choreo APIM publisher scenarios", () => {
     ComponentAPILifecycle.configureSecuritySettings(false, false, [], [], []);
     ComponentAPILifecycle.navigatePermissionManagementWindow();
     ComponentAPILifecycle.managePermissions(permissions, API_NAME);
+  });
+
+  it("Change API lifecycle to Published", () => {
     ComponentAPILifecycle.manageLifecycle();
+    ComponentAPILifecycle.publishWithoutConnector();
+    
   });
 
   it("Verify connector publishing ", () => {
