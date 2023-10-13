6776eba6f693c3f2f7f4ff99adbe59f23e56d229
chalindkodikara <chalindu.18@cse.mrt.ac.lk>
2023-10-13 12:28:13 +0530

    Remove alter permission in sql script

--- databases/scripts/choreo_cp_configuration_service_db_mssql.sql
+++ databases/scripts/choreo_cp_configuration_service_db_mssql.sql
@@ -9,7 +9,7 @@
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'choreo_configuration_service_admin_db_user')
 BEGIN
     CREATE USER [choreo_configuration_service_admin_db_user] with password = N'xxxxxxxxxxxxx'
-    GRANT ALTER, SELECT, INSERT, UPDATE, DELETE, EXECUTE ON DATABASE::choreo_configuration_service_db TO choreo_configuration_service_admin_db_user
+    GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON DATABASE::choreo_configuration_service_db TO choreo_configuration_service_admin_db_user
 END;
 GO
 
