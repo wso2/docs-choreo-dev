f05e8d7e24eae267a3e74d012e48025c1ad0d648
chalindkodikara <chalindu.18@cse.mrt.ac.lk>
2023-10-13 16:35:19 +0530

    Update user name

--- databases/scripts/choreo_cp_configuration_service_db_mssql.sql
+++ databases/scripts/choreo_cp_configuration_service_db_mssql.sql
@@ -8,8 +8,8 @@
 -- Create user
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'choreo_configuration_service_admin_db_user')
 BEGIN
-    CREATE USER [choreo_configuration_service_admin_db_user] with password = N'xxxxxxxxxxxxx'
-    GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON DATABASE::choreo_configuration_service_db TO choreo_configuration_service_admin_db_user
+    CREATE USER [choreo_configuration_service_db_user] with password = N'xxxxxxxxxxxxx'
+    GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON DATABASE::choreo_configuration_service_db TO choreo_configuration_service_db_user
 END;
 GO
 
