4fb3252767dd607402ffc4dbcdaa0f4b5ba9f82d
isuruPiyarathna <isurupi@wso2.com>
2023-11-04 13:07:29 +0530

    Fixes

--- e2e-tests-cypress/cypress/e2e-stable/console/1-component/1.11-bring-your-own-container.ts
+++ e2e-tests-cypress/cypress/e2e-stable/console/1-component/1.11-bring-your-own-container.ts
@@ -19,12 +19,10 @@ import { ComponentBuild } from "../../../support/console/pages/component/Functio
 import { TestHelper } from "../../../support/console/pages/component/common/test-helper";
 import { ComponentDeployPage } from "../../../support/console/pages/component/component-deploy";
 import { ComponentListingPage } from "../../../support/console/pages/component/component-listing-page";
-import { ComponentAPILifecycle } from "../../../support/console/pages/component/component-manage-page";
 import { ComponentOverviewPage } from "../../../support/console/pages/component/component-overview-page";
 import { ChoreoHomePage } from "../../../support/console/pages/home/home-page";
 import { LoginPage } from "../../../support/console/pages/login-page";
 import { ProjectListingPage } from "../../../support/console/pages/projects/projects-listing-page";
-import { GitHub } from "../../../support/github/github";
 import { ByocComponent } from "../../../support/interfaces/byoc-component";
 
 before(() => {
@@ -46,12 +44,12 @@ describe(`Verify BYOC functionality`, () => {
     ProjectListingPage.createNewProject(PROJECT_NAME, PROJECT_DESCRIPTION);
   });
 
-  it("Verify REST API component creation", () => {
+  it("Verify BYOC REST API component creation", () => {
     let componentData: ByocComponent = {
       name: REST_API_NAME,
       displayName: REST_API_NAME,
       accessibility: Enums.Accessibility.EXTERNAL,
-      componentType: Enums.DisplayType.byocService,
+      componentType: Enums.DisplayType.byocRestApi,
       description: "BYOC Component",
       labels: "",
       oasFilePath: "byoc-test/oas.yaml",
@@ -76,8 +74,6 @@ describe(`Verify BYOC functionality`, () => {
 
   it("Navigate to deployment", () => {
     ComponentListingPage.visitToAComponent(REST_API_NAME);
-    ComponentOverviewPage.navigateToBuild();
-    ComponentBuild.buildComponent();
     ComponentOverviewPage.navigateToDeploy();
   });
 
