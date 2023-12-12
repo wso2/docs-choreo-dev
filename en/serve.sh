e3d1e929fd6d22952736d96059bd5e2b90a617f4
Sachith Kasthuriarachchi <sachithka@wso2.com>
2023-12-12 16:02:55 +0530

    Create a function to check the status code

--- en/serve.sh
+++ en/serve.sh
@@ -1,9 +1,30 @@
 #! /bin/sh
+
+check_and_exit() {
+    local exit_status=$1
+    local err_msg=$2
+
+    if [ $exit_status -eq 1 ]; then
+        echo "[ERROR] $err_msg"
+        exit 1
+    fi
+}
+
 set -e
+
 mdspell  -n -a --en-us docs/**/*.md -d dictionary/en_US-large
 mdspell  -n -a --en-us docs/**/**/*.md -d dictionary/en_US-large
 mdspell  -n -a --en-us mkdocs.yml -d dictionary/en_US-large
 mdspell  -n -a --en-us theme/material/templates/home-page.html -d dictionary/en_US-large
+
 find docs/** -type f -name '*.md'  | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet
+exit_status="$?"
+check_and_exit "$exit_status" "deadlinks found!"
+
 find docs/**/* -type f -name '*.md' | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet
+exit_status="$?"
+check_and_exit "$exit_status" "deadlinks found!"
+
 mkdocs build -c
+exit_status="$?"
+check_and_exit "$exit_status" "build failed!"
