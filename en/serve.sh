f9400d1f74cc6261ad9492aa7703d21082ed9f09
Sachith Kasthuriarachchi <sachithka@wso2.com>
2023-12-12 16:16:13 +0530

    Use a for loop

--- en/serve.sh
+++ en/serve.sh
@@ -17,13 +17,14 @@ mdspell  -n -a --en-us docs/**/**/*.md -d dictionary/en_US-large
 mdspell  -n -a --en-us mkdocs.yml -d dictionary/en_US-large
 mdspell  -n -a --en-us theme/material/templates/home-page.html -d dictionary/en_US-large
 
-find docs/** -type f -name '*.md'  | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet
-exit_status="$?"
-check_and_exit "$exit_status" "deadlinks found!"
+readarray markdown_files < <(find docs/**/* -type f -name '*.md')
 
-find docs/**/* -type f -name '*.md' | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet
-exit_status="$?"
-check_and_exit "$exit_status" "deadlinks found!"
+for file in ${markdown_files[@]}; do
+  markdown-link-check "$file" -c ./markdown-link-check-config.json --quiet
+done
+
+# exit_status="$?"
+# check_and_exit "$exit_status" "deadlinks found!"
 
 mkdocs build -c
 exit_status="$?"
