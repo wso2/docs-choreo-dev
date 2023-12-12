3756c74bcf411441537a55b60a66a8e77073bb3d
Sachith Kasthuriarachchi <sachithka@wso2.com>
2023-12-12 15:53:33 +0530

    Remove exit condition

--- en/serve.sh
+++ en/serve.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-set -eo pipefail
+set -e
 mdspell  -n -a --en-us docs/**/*.md -d dictionary/en_US-large
 mdspell  -n -a --en-us docs/**/**/*.md -d dictionary/en_US-large
 mdspell  -n -a --en-us mkdocs.yml -d dictionary/en_US-large
 mdspell  -n -a --en-us theme/material/templates/home-page.html -d dictionary/en_US-large
-find docs/** -type f -name '*.md'  | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet || exit 1 
-find docs/**/* -type f -name '*.md' | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet || exit 1
+find docs/** -type f -name '*.md'  | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet
+find docs/**/* -type f -name '*.md' | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet
 mkdocs build -c
