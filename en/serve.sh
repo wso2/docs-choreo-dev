43673b8b7ef69ff09fb6713e96f18fa6410a7e14
SachithKasthuriarachchi <sachithnilmantha.97@gmail.com>
2023-12-12 22:07:09 +0530

    Reformat

--- en/serve.sh
+++ en/serve.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mdspell -V
-mdspell  -n -a --en-us docs/**/*.md -d dictionary/en_US-large
-mdspell  -n -a --en-us docs/**/**/*.md -d dictionary/en_US-large
-mdspell  -n -a --en-us mkdocs.yml -d dictionary/en_US-large
-mdspell  -n -a --en-us theme/material/templates/home-page.html -d dictionary/en_US-large
-find docs/** -type f -name '*.md'  | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet || exit 1 
-find docs/**/* -type f -name '*.md' | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json  --quiet || exit 1
+mdspell -n -a --en-us docs/**/*.md -d dictionary/en_US-large
+mdspell -n -a --en-us docs/**/**/*.md -d dictionary/en_US-large
+mdspell -n -a --en-us mkdocs.yml -d dictionary/en_US-large
+mdspell -n -a --en-us theme/material/templates/home-page.html -d dictionary/en_US-large
+find docs/** -type f -name '*.md' | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json --quiet || exit 1
+find docs/**/* -type f -name '*.md' | xargs -L1 markdown-link-check -c ./markdown-link-check-config.json --quiet || exit 1
 mkdocs build -c
 
 ls site/
