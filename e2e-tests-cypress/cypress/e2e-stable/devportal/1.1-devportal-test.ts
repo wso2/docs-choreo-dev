566a2f381c0b175c7093674c796c9f5c9a9fc776
shanakama <shanakama@wso2.com>
2024-02-15 12:05:13 +0530

    Address review suggesstions

--- e2e-tests-cypress/cypress/e2e-stable/devportal/1.1-devportal-test.ts
+++ e2e-tests-cypress/cypress/e2e-stable/devportal/1.1-devportal-test.ts
@@ -18,14 +18,17 @@ import { Project } from "../../support/console/entities/project/project";
 import { Proxy } from "../../support/console/entities/component/proxy-component";
 import { devPortal } from "../../support/console/devportal";
 import { Application } from "../../support/console/entities/application/application";
+import { LoginPage as DevportalLoginPage } from "../../support/devportal/pages/login/login-page";
+import { DevPortalHomePage } from "../../support/devportal/pages/home/home-page";
+
+let application: Application;
+let proxy: Proxy;
 
 describe("API overview comment and rating scenario", () => {
   const PROJECT_DESCRIPTION = "sample oas flow scenario";
   const OPERATION_USERS = "intensity";
 
   let project: Project;
-  let proxy: Proxy;
-  let application: Application;
 
   after(() => {
     console.logout();
@@ -113,6 +116,10 @@ describe("API overview comment and rating scenario", () => {
     application = proxy.createApplication_DevPortal();
   });
 
+  it("Share application with another user", () => {
+    application.shareApplication(application.getName);
+  });
+
   it("Generate subscription credentials", () => {
     application.generateCredentials(Enums.Environment.SANDBOX);
     application.generateCredentials(Enums.Environment.PRODUCTION);
@@ -122,13 +129,30 @@ describe("API overview comment and rating scenario", () => {
     application.addSubscription(proxy.getName());
   });
 
-  it("Delete a consumer application", () => {
-    proxy.deleteApplication_DevPortal(application);
-  });
-
   it("Verify suspending Dev deployed component", () => {
     proxy.navigateToComponentInConsole();
     proxy.stopDeployment();
     proxy.stopPromotion();
   });
+});
+
+
+describe("Check application with shared user", () => {
+
+  before(() => {
+    DevportalLoginPage.loginToDevportalAsInvitedUser();
+  });
+
+  after(() => {
+    DevPortalHomePage.logout();
+  });
+
+  it("Check shared application", () => {
+      let appName = application.getName();
+      cy.get('[data-testid="applications-appbar-btn"]').click();
+      cy.get('[data-testid="search-btn"]').trigger("mouseover");
+      cy.get('[data-testid="search-app"] [placeholder="Search"]').type(appName);
+      cy.get('[data-testid="application-list-a"] > [value="a"]').should("exist");
+  });
+
 });
