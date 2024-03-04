73ce3e0edf78c0d15c7c8d2917b9622d68f2f4a7
uvindra <uvindra_dj@yahoo.com>
2024-03-04 10:24:53 +0530

    Update 1.8 to support API Config feature

--- e2e-tests-cypress/cypress/support/console/entities/component/proxy-component.ts
+++ e2e-tests-cypress/cypress/support/console/entities/component/proxy-component.ts
@@ -18,6 +18,7 @@ import { mixinProxyDeploy } from "../../features/deploy/deploy-proxy";
 import { mixinDevelop } from "../../features/develop/develop";
 import { mixinTestProxy } from "../../features/test/test-proxy";
 import { mixinManage } from "../../features/manage/manage";
+import { Utils } from "../../../commons/utils";
 
 export class Proxy extends mixinDevelop(
   mixinManage(mixinProxyDeploy(mixinTestProxy(Component)))
@@ -163,16 +164,29 @@ export class Proxy extends mixinDevelop(
     this._verifyConsumer(appName);
   }
 
-  disableSecurityInDev(resource: string) {
-    this._disableSecurity(this, Enums.Environment.DEVELOPMENT, resource);
+  disableSecurity(method: Enums.HTTPMethod, resource: string) {
+    this._disableSecurity(this, Enums.Environment.PRODUCTION, method, resource);
   }
 
-  disableSecurityInProd(resource: string) {
-    this._disableSecurity(this, Enums.Environment.PRODUCTION, resource);
+  disableSecurityInDev(method: Enums.HTTPMethod, resource: string) {
+    this._disableSecurity(
+      this,
+      Enums.Environment.DEVELOPMENT,
+      method,
+      resource
+    );
+  }
+
+  disableSecurityInProd(method: Enums.HTTPMethod, resource: string) {
+    this._disableSecurity(this, Enums.Environment.PRODUCTION, method, resource);
   }
 
   updateAccessMode(accessMode: Enums.Accessibility) {
     this._updateAccessMode(this, accessMode);
+  }
+
+  updateAccessModeAndDeploy(accessMode: Enums.Accessibility) {
+    this._deploy(this, accessMode);
   }
 
   updateApiVisibility(visibility: ApiVisibility) {
