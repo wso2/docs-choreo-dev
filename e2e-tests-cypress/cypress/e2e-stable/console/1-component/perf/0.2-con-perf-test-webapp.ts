cdb142a00c387556a49f3b416462b201ee59d541
isuruPiyarathna <isurupi@wso2.com>
2024-03-12 16:54:49 +0530

    small change

---
+++ e2e-tests-cypress/cypress/e2e-stable/console/1-component/perf/0.2-con-perf-test-webapp.ts
@@ -0,0 +1,221 @@
+import { GRAPHQL_URL } from "../../../../support/commons/urls";
+import { Enums } from "../../../../support/commons/enums";
+import { login } from "../../../../support/console/entities/login/login";
+import {
+  Project,
+  RepoInfo,
+  WebAppInfo,
+} from "../../../../support/console/entities/project/project";
+import { console } from "../../../../support/console/console";
+import { CSVWriter } from "../../../../support/commons/csvwriter";
+import { WebApp } from "../../../../support/console/entities/component/webapp-component";
+import { PERF_INTERCEPT_WAIT_TIME } from "../../../../support/commons/timeouts";
+
+after(() => {
+  console.logout();
+});
+
+describe("Multiple User Logins", () => {
+  let project: Project;
+  let webApp: WebApp;
+  const PROJECT_DESCRIPTION = "Cypress WebApp Perf Test Project";
+  const username = Cypress.env("perfUsername");
+  let csv;
+
+  const repoInfo: RepoInfo = {
+    url: "https://github.com/choreo-test-apps/choreo-examples",
+    branch: "main",
+    dockerContext:
+      "cloud-native-app-developer/reading-list-front-end-with-managed-auth",
+  };
+
+  const webAppInfo: WebAppInfo = {
+    webAppType: "React",
+    webAppBuildCommand: "npm install && npm run build",
+    webAppPackageManagerVersion: "18",
+    webAppOutputDirectory: "dist",
+  };
+
+  before(() => {
+    csv = new CSVWriter(username);
+  });
+
+  it("Login with multiple users concurrently", () => {
+    login.perfLogin();
+  });
+
+  it("Creating a project", () => {
+    cy.intercept(
+      {
+        method: "POST",
+        url: GRAPHQL_URL,
+      },
+      (req) => {
+        // Convert req.body to a string
+        const requestBodyString = JSON.stringify(req.body);
+        if (requestBodyString.includes("createProject")) {
+          req.alias = "createProjectRequest";
+        }
+      }
+    );
+
+    project = console.createNewProject(PROJECT_DESCRIPTION);
+    csv.writeInterceptionResultsToCsv("Creating a project");
+
+    cy.wait("@createProjectRequest", PERF_INTERCEPT_WAIT_TIME).then(
+      (interception) => {
+        try {
+          expect(interception).to.have.property("response");
+          expect(interception.response?.statusCode).to.equal(200);
+          csv.writeInterceptionResultsToCsv("Creating a project");
+
+          const responseBody = interception.response?.body;
+          expect(responseBody).to.have.property("data");
+
+          const createProject = responseBody?.data?.createProject;
+          expect(createProject, "createProject is missing in the response").to
+            .exist;
+          expect(createProject)
+            .to.have.property("region")
+            .that.equals("US-CDP-2");
+          expect(createProject).to.have.property("name");
+        } catch (error: any) {
+          // Log assertion errors and proceed
+          cy.log(`Assertion error: ${error.message}`);
+          csv.writeInterceptionResultsToCsv(
+            "Creating a project",
+            error.message
+          );
+        }
+      }
+    );
+  });
+
+  it("Verify WebApp component creation", () => {
+    cy.intercept(
+      {
+        method: "POST",
+        url: GRAPHQL_URL,
+      },
+      (req) => {
+        const requestBodyString = JSON.stringify(req.body);
+        if (requestBodyString.includes("createByocComponent")) {
+          req.alias = "componentCreationRequest";
+        }
+      }
+    );
+
+    // Create a webapp component
+    project
+      .createWebAppComponent(Enums.Accessibility.EXTERNAL, repoInfo, webAppInfo)
+      .then((app: WebApp) => {
+        project.visitComponent(app.getName());
+        webApp = app;
+      });
+
+    cy.wait("@componentCreationRequest", PERF_INTERCEPT_WAIT_TIME).then(
+      (interception) => {
+        try {
+          expect(interception).to.have.property("response");
+          expect(interception.response?.statusCode).to.equal(200);
+          const responseBody = interception.response?.body;
+          expect(responseBody).to.have.property("data");
+
+          const createComponent = responseBody?.data?.createComponent;
+          expect(
+            createComponent,
+            "createWebappComponent is missing in the response"
+          ).to.exist;
+          expect(createComponent).to.have.property("orgId");
+          expect(createComponent).to.have.property("projectId");
+          expect(createComponent).to.have.property("handler");
+        } catch (error: any) {
+          cy.log(`Assertion error: ${error.message}`);
+          csv.writeInterceptionResultsToCsv(
+            "Verify Webapp service component creation",
+            error.message
+          );
+        }
+      }
+    );
+  });
+
+  it("Build the component", () => {
+    cy.intercept(
+      {
+        method: "POST",
+        url: GRAPHQL_URL,
+      },
+      (req) => {
+        const requestBodyString = JSON.stringify(req.body);
+        if (requestBodyString.includes("deployComponent")) {
+          req.alias = "buildComponentRequest";
+        }
+      }
+    );
+
+    webApp.build();
+    cy.wait("@buildComponentRequest", PERF_INTERCEPT_WAIT_TIME).then(
+      (interception) => {
+        try {
+          expect(interception).to.have.property("response");
+          expect(interception.response?.statusCode).to.equal(200);
+          const responseBody = interception.response?.body;
+          expect(responseBody).to.have.property("data");
+
+          const deploymentStatuses =
+            responseBody?.data?.deploymentStatusByVersion;
+          expect(
+            deploymentStatuses,
+            "deploymentStatuses is missing or empty"
+          ).to.exist.and.to.be.an("array").and.not.empty;
+
+          deploymentStatuses.forEach((status) => {
+            expect(status).to.have.property("status").that.equals("completed");
+            expect(status)
+              .to.have.property("conclusion")
+              .that.equals("success");
+          });
+        } catch (error: any) {
+          cy.log(`Assertion error: ${error.message}`);
+          csv.writeInterceptionResultsToCsv(error.message);
+        }
+      }
+    );
+  });
+
+  it("Deploying to dev", () => {
+    cy.intercept(
+      {
+        method: "POST",
+        url: GRAPHQL_URL,
+      },
+      (req) => {
+        const requestBodyString = JSON.stringify(req.body);
+        if (requestBodyString.includes("deployDeploymentTrack")) {
+          req.alias = "deployComponentRequest";
+        }
+      }
+    );
+
+    webApp.deployToDevWithAuthConfiguration();
+
+    cy.wait("@deployComponentRequest", PERF_INTERCEPT_WAIT_TIME).then(
+      (interception) => {
+        try {
+          expect(interception).to.have.property("response");
+          expect(interception.response?.statusCode).to.equal(200);
+
+          const responseBody = interception.response?.body;
+          expect(responseBody).to.have.property("data");
+
+          const deploymentResponse = responseBody?.data?.deployDeploymentTrack;
+          expect(deploymentResponse).to.equal("Sucessfully deployed");
+        } catch (error: any) {
+          cy.log(`Assertion error: ${error.message}`);
+          csv.writeInterceptionResultsToCsv(error.message);
+        }
+      }
+    );
+  });
+});
