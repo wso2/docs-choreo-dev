79f0dc85821a66b4e318e325d76cf1ad23584592
uvindra <uvindra_dj@yahoo.com>
2024-03-22 09:16:56 +0530

    Fix for 3.2

--- e2e-tests-cypress/cypress/support/console/pages/component/common/onpremkey-components.ts
+++ e2e-tests-cypress/cypress/support/console/pages/component/common/onpremkey-components.ts
@@ -29,7 +29,7 @@ export class OnPremkeyComponent {
     cy.get('[data-testid="on-prem-key-copy-btn"]', { timeout: 120000 }).should(
       "be.visible"
     );
-    cy.get('[data-testid="dialog-close-icon"]').eq(0).should("exist").click();
+    Utils.getRenderedElement('[data-testid="dialog-close-icon"]').eq(0).click();
   }
 
   static editOnPremKey() {
