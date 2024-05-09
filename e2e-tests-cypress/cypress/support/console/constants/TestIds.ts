368a3da98c77d6631aa8b5e67005a13977d7af19
isuruPiyarathna <isurupi@wso2.com>
2024-05-09 15:21:16 +0530

    new changes for perf

--- e2e-tests-cypress/cypress/support/console/constants/TestIds.ts
+++ e2e-tests-cypress/cypress/support/console/constants/TestIds.ts
@@ -373,4 +373,5 @@ export namespace TestIds {
   export const rightDrawer = '[data-cyid="Overlay-right-drawer"]';
   export const refreshComponentListIconButton =
     '[data-cyid="refresh-components-icon-button"]';
+    
 }
