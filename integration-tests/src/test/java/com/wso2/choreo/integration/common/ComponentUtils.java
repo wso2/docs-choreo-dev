c1571a33b35a87140d9e99a0ba4cd87e71728240
Janaka Sandaruwan <janakasandaruwan1996@gmail.com>
2024-05-21 17:16:54 +0530

    Fix stage test failure

--- integration-tests/src/test/java/com/wso2/choreo/integration/common/ComponentUtils.java
+++ integration-tests/src/test/java/com/wso2/choreo/integration/common/ComponentUtils.java
@@ -672,7 +672,7 @@ public class ComponentUtils {
 
         HttpClient apimClient = citrusClients.get(Endpoints.STS_ENDPOINT);
         String query = "context:/" + Configuration.getConfig(ConfigDefinition.TEST_CHOREO_ORG_UUID) + "/"
-                + project.getHandler() + "/" + component.getHandler() + "/v1.0";
+                + project.getHandler() + "/" + component.getName() + "/v1.0";
         ApiManager.searchAPIByQuery(runner, apimClient, accessToken, query);
 
         List<ComponentDeploymentStatusDTO> promotionStatus = null;
