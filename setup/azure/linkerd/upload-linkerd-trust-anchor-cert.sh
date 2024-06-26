9c8654af57e1f26bdb260d2571a42c56042c6a76
SachithKasthuriarachchi <sachithka@wso2.com>
2024-06-26 11:34:38 +0530

    Update steps to generate webhook TLS cert for linkerd

--- setup/azure/linkerd/upload-linkerd-trust-anchor-cert.sh
+++ setup/azure/linkerd/upload-linkerd-trust-anchor-cert.sh
@@ -17,7 +17,7 @@ generate_sample_spc() {
 apiVersion: secrets-store.csi.x-k8s.io/v1
 kind: SecretProviderClass
 metadata:
-  name: linkerd-trust-anchor
+  name: linkerd-tls
 spec:
   provider: azure
   secretObjects:
@@ -28,6 +28,13 @@ spec:
           key: tls.key
         - objectName: linkerd-trust-anchor
           key: tls.crt
+    - secretName: webhook-issuer-tls
+      type: kubernetes.io/tls
+      data:
+        - objectName: linkerd-webhook
+          key: tls.key
+        - objectName: linkerd-webhook
+          key: tls.crt
   parameters:
     keyvaultName: "$keyvault_name"
     objects: |
@@ -35,7 +42,11 @@ spec:
         - |
           objectName: linkerd-trust-anchor
           objectType: secret
-          objectVersion: "$object_version"
+          objectVersion: "$trust_anchor_object_version"
+        - |
+          objectName: linkerd-webhook
+          objectType: secret
+          objectVersion: "$webhook_object_version"
 EOF
 }
 
@@ -69,17 +80,33 @@ if [[ -z "$keyvault_name" ]]; then
   exit 1
 fi
 
-step certificate create root.linkerd.cluster.local ca.crt ca.key \
+mkdir -p linkerd-trust-anchor
+mkdir -p linkerd-webhook
+
+step certificate create root.linkerd.cluster.local linkerd-trust-anchor/ca.crt linkerd-trust-anchor/ca.key \
   --profile root-ca --no-password --insecure
 
-openssl pkcs12 -export -out linkerd-trust-anchor.pfx -inkey ca.key -in ca.crt -passout pass:""
-output=$(az keyvault certificate import \
-    --file linkerd-trust-anchor.pfx --name linkerd-trust-anchor --vault-name "$keyvault_name" --password "")
+step certificate create webhook.linkerd.cluster.local linkerd-webhook/ca.crt linkerd-webhook/ca.key \
+  --profile root-ca --no-password --insecure --san webhook.linkerd.cluster.local
 
-id=$(echo "$output" | jq '.id' | tr -d '"')
-object_version=${id##*/}
-echo "[INFO] Successfully uploaded the trust anchor certificate to Azure Keyvault"
-echo "[INFO] Use the following sample SPC to configure the SecretProviderClass for Linkerd trust anchor certificate:"
+openssl pkcs12 -export -out linkerd-trust-anchor/linkerd-trust-anchor.pfx \
+  -inkey linkerd-trust-anchor/ca.key -in linkerd-trust-anchor/ca.crt -passout pass:""
+openssl pkcs12 -export -out linkerd-webhook/linkerd-webhook.pfx \
+  -inkey linkerd-webhook/ca.key -in linkerd-webhook/ca.crt -passout pass:""
+
+trust_anchor_output=$(az keyvault certificate import \
+    --file linkerd-trust-anchor/linkerd-trust-anchor.pfx --name linkerd-trust-anchor --vault-name "$keyvault_name" --password "")
+webhook_output=$(az keyvault certificate import \
+    --file linkerd-webhook/linkerd-webhook.pfx --name linkerd-webhook --vault-name "$keyvault_name" --password "")
+
+trust_anchor_id=$(echo "$trust_anchor_output" | jq '.id' | tr -d '"')
+webhook_id=$(echo "$webhook_output" | jq '.id' | tr -d '"')
+
+trust_anchor_object_version=${trust_anchor_id##*/}
+webhook_object_version=${webhook_id##*/}
+echo "[INFO] Successfully uploaded the trust anchor and webhook certificate to Azure Keyvault"
+echo "[INFO] Use the following sample SPC to configure the SecretProviderClass for Linkerd trust anchor and webhook certs"
 generate_sample_spc
 
-rm -f ca.crt ca.key linkerd-trust-anchor.pfx
+rm -R linkerd-trust-anchor
+rm -R linkerd-webhook
