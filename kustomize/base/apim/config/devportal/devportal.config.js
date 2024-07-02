af151f618c45c7ce4eb38f84959a4671e315c9fa
SLahiruC <40932779+slahirucd7@users.noreply.github.com>
2024-07-02 09:38:06 +0530

    custom subscription policy handling org addition

--- kustomize/base/apim/config/devportal/devportal.config.js
+++ kustomize/base/apim/config/devportal/devportal.config.js
@@ -90,5 +90,6 @@ if (isCustomDomain) {
         permissionPortalOrgIdParam: 'orgId',
         intelligentRoutingFeatureEnabled: true,
         choreoInbuiltKeyManagerFeatureEnabled: false,
+        customSubscriptionPolicyHandlingOrg: '{{.CUSTOM_SUBSCRIPTION_POLICY_HANDLING_ORG}}',
     }
 }
