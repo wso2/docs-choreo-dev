5f0b3fe26d56fb174654966c7384d297213db075
Heshitha <drifftr@gmail.com>
2024-08-16 13:10:19 +0530

    Fix test failure listEnvironments_ApimProxyDeployerElevatedAccessCheck

--- integration-tests/src/test/java/com/wso2/choreo/integration/tests/security/apim/ProxyDeployer/ApimProxyDeployerElevatedAccessCheck.java
+++ integration-tests/src/test/java/com/wso2/choreo/integration/tests/security/apim/ProxyDeployer/ApimProxyDeployerElevatedAccessCheck.java
@@ -149,7 +149,7 @@ public class ApimProxyDeployerElevatedAccessCheck extends TestNGCitrusSpringSupp
     @Test
     @CitrusTest
     public void listEnvironments_ApimProxyDeployerElevatedAccessCheck() throws Exception {
-        HttpClient choreoCPTestClient = citrusClients.get(Endpoints.CHOREO_ENDPOINT);
+        HttpClient choreoCPTestClient = citrusClients.get(Endpoints.CHOREO_NEW_APP_SERVICE_ENDPOINT);
         String requestUrl = "/devops/1.0.0/api/v1/organizations/" + orgUuid + "/environments?organization_id=" +
                 orgUuid + "&project_id=" + projectId + "&include=environment_clusters";
         SecurityUtils.elevatedAccessCheckForGetRequests(this, choreoCPTestClient, requestUrl, accessToken);
