209686ea5ff38184f220ee562a318469fd109693
kaveeshadinamidu <kaveeshadi@wso2.com>
2024-08-26 11:51:00 +0530

    Scripts created for db export and import

---
+++ scripts/backup-and-restore-mssql-db/container-setup-and-export.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2024, WSO2 LLC (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+source credentials.sh
+
+
+key=$(az storage account keys list --account-name $storage --resource-group $resourceGroup --subscription $subscriptionId -o json --query [0].value | tr -d '"')
+
+echo "Getting the available database list of server : ${server} and elastic pool ${elasticPool}"
+databases=$(az sql db list --subscription $subscriptionId  --resource-group $resourceGroup --elastic-pool $elasticPool --server $server | jq -r '.[].name')
+
+echo "Writing the available databases to a text file"
+for line in "${databases[@]}"; do
+    echo "$line" >> db-names.txt
+done
+
+echo "Completed the file writing to the db-names.txt file"
+sleep 5
+
+echo "Creating the container to store the db-names.txt file"  
+az storage container create --name "dbnames" --account-key $key --account-name $storage --subscription $subscriptionId
+echo "Container created succesfully"
+sleep 5
+
+each "Uploading the created file to the dbnames container in ${storage} Storage Account"
+az storage blob upload  --account-name $storage --name "db-names.txt" --account-key $key  --file db-names.txt --blob-url "https://${storage}.blob.core.windows.net/dbnames/db-names.txt"
+echo "Upload completed"
+
+# create a storage containers for databases
+for element in "${databases[@]}"
+do
+    output_container=$(echo "$element" | sed 's/_//g')
+    echo "Creating "${output_container}container" on $storage for $element database"
+    az storage container create --name "${output_container}container" --account-key $key --account-name $storage --subscription $subscriptionId
+    sleep 5
+done
+echo "Creating container for the databases are completed"
+
+# Exporting the databases
+for element in "${databases[@]}"
+do
+    output_container=$(echo "$element" | sed 's/_//g')
+    echo "Executing the db export for the database ${element} to the storage container ${output_container}container"
+    az sql db export --admin-password $password --admin-user $login --storage-key $key --storage-key-type StorageAccessKey --storage-uri "https://$storage.blob.core.windows.net/${output_container}container/$bacpac" --name $element --resource-group $resourceGroup --server $server --subscription $subscriptionId &
+    sleep 5
+done
+
---
+++ scripts/backup-and-restore-mssql-db/create-db-and-import.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2024, WSO2 LLC (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+source credentials.sh
+
+echo "Generating the key to access the storage account"
+key=$(az storage account keys list --account-name $storage --resource-group $resourceGroup --subscription $subscriptionId -o json --query [0].value | tr -d '"')
+
+echo "Downloading the db-names.txt file to get the availale db names for the import process"
+az storage blob download -c dbnames -n db-names.txt --account-name $storage --account-key $key > db-names.txt
+
+echo "Completed downloading the file"
+
+databases=()
+
+# Read the file line by line and add each line to the array
+echo "Reading the file data and adding the database names to an array"
+while IFS= read -r line; do
+  lines+=("$line")
+done < db-names.txt
+
+echo "Creating the elastic pool to add the databases"
+az sql elastic-pool create --name $elasticPool --resource-group $resourceGroup --subscription $subscriptionId --server $server -e Standard -f Gen5 --max-size 120GB -z false
+
+# Creating the databases and importing the data to it
+for element in "${databases[@]}"
+do
+    echo "Creating a database with the name ${element}"
+    az sql db create --name "${element}" --elastic-pool $elasticPool --resource-group $resourceGroup --subscription $subscriptionId --server $server --backup-storage-redundancy Local > "${element}.json"
+    echo "DB Created succesfully!"
+    output_container=$(echo "$element" | sed 's/_//g')
+    sleep 5
+    echo "Importing the data to the ${element} database from the storage account"
+    az sql db import --auth-type SQL -s $server -n "${element}_backup" -g $resourceGroup -p $password -u $login --storage-key $key --storage-key-type StorageAccessKey --storage-uri "https://$storage.blob.core.windows.net/${output_container}container/$bacpac" --subscription $subscriptionId &
+    sleep 5
+done
+
