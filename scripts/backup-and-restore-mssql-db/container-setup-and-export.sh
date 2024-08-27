6daad15e97810b6c87821d3965384d2dbc2e7742
kaveeshadinamidu <kaveeshadi@wso2.com>
2024-08-27 14:17:30 +0530

    Linting issues fixed

--- scripts/backup-and-restore-mssql-db/container-setup-and-export.sh
+++ scripts/backup-and-restore-mssql-db/container-setup-and-export.sh
@@ -38,7 +38,7 @@ echo "Upload completed"
 # create a storage containers for databases
 for element in "${databases[@]}"
 do
-    output_container=$element | sed 's/_//g'
+    output_container=$($element | sed 's/_//g')
     echo "Creating ${output_container}container on $storage for $element database"
     az storage container create --name "${output_container}container" --account-key "$key" --account-name "$storage" --subscription "$subscriptionId"
     sleep 5
@@ -48,7 +48,7 @@ echo "Creating container for the databases are completed"
 # Exporting the databases
 for element in "${databases[@]}"
 do
-    output_container=$element | sed 's/_//g'
+    output_container=$($element | sed 's/_//g')
     echo "Executing the db export for the database ${element} to the storage container ${output_container}container"
     az sql db export --admin-password "$password" --admin-user "$login" --storage-key "$key" --storage-key-type StorageAccessKey --storage-uri "https://$storage.blob.core.windows.net/${output_container}container/$bacpac" --name "$element" --resource-group "$resourceGroup" --server "$server" --subscription "$subscriptionId" > "${element}.json" &
     sleep 5
