66af89768750925898b19e68baa10ed0bc1317e3
kaveeshadinamidu <kaveeshadi@wso2.com>
2024-08-27 10:45:27 +0530

    Fixes added

--- scripts/backup-and-restore-mssql-db/container-setup-and-export.sh
+++ scripts/backup-and-restore-mssql-db/container-setup-and-export.sh
@@ -38,8 +38,8 @@ echo "Upload completed"
 # create a storage containers for databases
 for element in "${databases[@]}"
 do
-    output_container=$(echo "$element" | sed 's/_//g')
-    echo "Creating "${output_container}container" on "$storage" for "$element" database"
+    output_container=$(echo ${"$element" | sed 's/_//g'})
+    echo "Creating ${output_container}container on "$storage" for "$element" database"
     az storage container create --name "${output_container}container" --account-key "$key" --account-name "$storage" --subscription "$subscriptionId"
     sleep 5
 done
