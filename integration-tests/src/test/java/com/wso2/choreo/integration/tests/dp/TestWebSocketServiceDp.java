414c79c3093a1a9d94086d55e082693b99bd802b
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2025-04-03 10:24:44 +0530

    Add integration test for websocket publishing

--- integration-tests/src/test/java/com/wso2/choreo/integration/tests/dp/TestWebSocketServiceDp.java
+++ integration-tests/src/test/java/com/wso2/choreo/integration/tests/dp/TestWebSocketServiceDp.java
@@ -1,7 +1,9 @@
 package com.wso2.choreo.integration.tests.dp;
 
 import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.exceptions.ValidationException;
 import com.consol.citrus.http.client.HttpClient;
+import com.wso2.choreo.integration.apis.apimanager.ApiManager;
 import com.wso2.choreo.integration.common.ComponentFlavour;
 import com.wso2.choreo.integration.common.ComponentUtils;
 import com.wso2.choreo.integration.common.Endpoints;
@@ -10,17 +12,20 @@ import com.wso2.choreo.integration.common.choreoproject.ChoreoComponent;
 import com.wso2.choreo.integration.common.choreoproject.ChoreoProject;
 import com.wso2.choreo.integration.common.utils.NameGenerator;
 import com.wso2.choreo.integration.config.Constant;
+import com.wso2.choreo.integration.models.endpoints.Endpoint;
 import com.wso2.choreo.integration.models.GraphqlDTO;
 import com.wso2.choreo.integration.models.apimanager.KeyData;
 import com.wso2.choreo.integration.models.code.Repository;
 import com.wso2.choreo.integration.models.environments.Environment;
 import com.wso2.choreo.integration.models.graphql.ComponentDeploymentStatusDTO;
+import com.wso2.choreo.integration.models.response.Response;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.springframework.http.HttpStatus;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -29,6 +34,7 @@ import java.util.Map;
 public class TestWebSocketServiceDp extends TestBase {
 
     private String accessToken;
+    private String apiId;
 
     @Autowired
     Map<Endpoints, HttpClient> citrusClients;
@@ -80,6 +86,14 @@ public class TestWebSocketServiceDp extends TestBase {
         List<ComponentDeploymentStatusDTO> statusDTO = ComponentUtils.promoteComponent(this, citrusClients,
                 accessToken, dp.getChoreoComponent(), dp.getEnvironments(), ComponentFlavour.STANDARD, dp.getChoreoProject());
         dp.setPromoteStatusDTO(statusDTO);
+        int promotionStatusListSize =  statusDTO.size();
+        ComponentDeploymentStatusDTO lastpromotionStatus = statusDTO.get(promotionStatusListSize-1);
+
+        List<Endpoint> endpoints = ComponentUtils.getEndpoints(this,citrusClients,accessToken, dp.getChoreoComponent(),
+                lastpromotionStatus);
+
+        Endpoint endpoint = endpoints.get(0);
+        apiId = endpoint.getApimId();
     }
 
     @Test(dependsOnMethods = {"createUserManagedComponentFor_TestWebSocketServiceDp"}, dataProvider = "dps")
@@ -88,5 +102,12 @@ public class TestWebSocketServiceDp extends TestBase {
         Pair<String, KeyData> invokeData = ComponentUtils.getInvokeInfo(this, citrusClients, accessToken, dp.getChoreoComponent(),
                 dp.getDeploymentStatusDTO(), dp.getEnvironments());
         ComponentUtils.invokeWSApi(this, invokeData.getRight().getApikey(), invokeData.getLeft(), "/");
+    }
+
+    public void publishWebSocketAPItoDevPortal_TestWebSocketServiceDp(DataProviderWrapper dp) throws Exception {
+        Response resp = ApiManager.changeLifeCycle(apiId, "Publish", accessToken);
+        if (resp.getStatusCode() != HttpStatus.OK.value()) {
+            throw new ValidationException("Failed to publish the API");
+        }
     }
 }
